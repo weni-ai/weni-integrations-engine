ddf769d6bf0ce8d3fe82a1357c758f1c4c4c9843
Sandro-Meireles <playploygames@gmail.com>
2021-06-08 15:58:08 -0300

    feat: Added accounts django app

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Createds
+    "marketplace.accounts",
 ]
 
 MIDDLEWARE = [
