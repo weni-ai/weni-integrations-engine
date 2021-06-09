bab53f833a0b486edaf0953f804969cc832b5ad5
Sandro-Meireles <playploygames@gmail.com>
2021-06-09 15:28:28 -0300

    fix: Adjusted INSTALLED_APPS comment

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -46,8 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    # Createds
+    # marketplace apps
     "marketplace.accounts",
 ]
 
