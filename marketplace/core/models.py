b38b26c798e6a75944e12a469f816e514d57f1e2
Sandro-Meireles <playploygames@gmail.com>
2021-06-09 15:32:13 -0300

    fix: Adjusted model labels

--- marketplace/core/models.py
+++ marketplace/core/models.py
@@ -7,8 +7,8 @@ from django.utils.translation import ugettext_lazy as _
 class AbstractBaseModel(models.Model):
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    criado_on = models.DateTimeField(_("Criado em"), editable=False, auto_now_add=True)
-    modified_on = models.DateTimeField(_("Modificado em"), auto_now=True)
+    created_on = models.DateTimeField(_("Created on"), editable=False, auto_now_add=True)
+    modified_on = models.DateTimeField(_("Modified on"), auto_now=True)
 
     class Meta:
         abstract = True
