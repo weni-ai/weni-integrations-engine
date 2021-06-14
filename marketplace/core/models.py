f666eeebb4fa0830428592a5e0a360f5b5e71fcb
Sandro-Meireles <playploygames@gmail.com>
2021-06-14 17:33:15 -0300

    feat: Added created_by and modified_by on AbstractBaseModel

--- marketplace/core/models.py
+++ marketplace/core/models.py
@@ -10,5 +10,25 @@ class AbstractBaseModel(models.Model):
     created_on = models.DateTimeField(_("Created on"), editable=False, auto_now_add=True)
     modified_on = models.DateTimeField(_("Modified on"), auto_now=True)
 
+    created_by = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.PROTECT,
+        related_name="created_%(class)ss",
+        editable=False,
+    )
+    modified_by = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.PROTECT,
+        related_name="modified_%(class)ss",
+        blank=True,
+        null=True,
+    )
+
+    def save(self):
+        if self.id and not self.modified_by:
+            raise ValueError("modified_by can't be None")
+
+        super().save()
+
     class Meta:
         abstract = True
