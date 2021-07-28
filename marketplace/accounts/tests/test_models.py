e9ef0ab7346230e3da2867088e3ee58aa71b456a
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-07-28 12:53:53 -0300

    feat: Add tests to extra_fields on User.objects.create_superuser

--- marketplace/accounts/tests/test_models.py
+++ marketplace/accounts/tests/test_models.py
@@ -21,6 +21,14 @@ class TestUserCreate(TestCase):
         self.assertTrue(user.is_active)
         self.assertEqual(self.user_info["email"], user.email)
 
+    def test_create_super_user_without_is_staff(self):
+        with self.assertRaisesMessage(ValueError, "Superuser must have is_staff=True."):
+            User.objects.create_superuser(**self.user_info, is_staff=False)
+
+    def test_create_super_user_without_is_superuser(self):
+        with self.assertRaisesMessage(ValueError, "Superuser must have is_superuser=True."):
+            User.objects.create_superuser(**self.user_info, is_superuser=False)
+
     def test_empty_email(self):
         user_info = self.user_info
         user_info.update(email="")
