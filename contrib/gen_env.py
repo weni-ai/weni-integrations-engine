986d20de77dbb73d5ff6b976a3624d5464f97182
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-07-28 11:39:39 -0300

    fix: Adjust function that generates a random secret key

--- contrib/gen_env.py
+++ contrib/gen_env.py
@@ -4,17 +4,14 @@ OBS: Run in development environment only
 """
 
 import os
-from string import ascii_letters, digits, punctuation
 
-from django.utils.crypto import get_random_string
+from django.core.management.utils import get_random_secret_key
 
-
-chars = ascii_letters + digits + punctuation
 
 CONFIG_STRING = f"""
 DEBUG=True
 ALLOWED_HOSTS=\"*\"
-SECRET_KEY=\"{get_random_string(50, chars)}\"
+SECRET_KEY=\"{get_random_secret_key()}\"
 DATABASE_URL=\"postgresql://marketplace:marketplace@localhost:5432/marketplace\"
 LANGUAGE_CODE=\"en-us\"
 TIME_ZONE=\"America/Maceio\"
