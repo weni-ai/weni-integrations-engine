4c5508bd5872005a74581a17d720b08ad4c219ac
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-07-28 12:56:50 -0300

    feat: Add tests to types.__init__._get_modules

---
+++ marketplace/applications/types/tests/test_types_init.py
@@ -0,0 +1,28 @@
+import os
+
+from django.test import TestCase
+
+from marketplace.applications import types
+
+
+class TestWongModuleImport(TestCase):
+    """
+    Tests if "_get_modules" will return any error when trying to import a file
+    """
+
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "test_wrong_module.txt")
+
+    def setUp(self):
+        super().setUp()
+
+        with open(self.file_path, "w"):
+            ...
+
+    def tearDown(self):
+        super().setUp()
+
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+
+    def test_wrong_module_not_appars_on_get_modules(self):
+        list(types._get_modules())
