a8d69e2c097b64df3cb1da95b2fb7fbc6d6bd43b
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-08-04 11:58:32 -0300

    fix: Adjust collectstatic on docker entrypoint and default variables on docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running collectstatic"
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 echo "Starting server"
 gunicorn marketplace.wsgi -c gunicorn.conf.py
