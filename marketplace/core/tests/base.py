24080b436dc0b9724d46aae3e3b9f5eca3374f33
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-08-09 16:54:56 -0300

    Add new HTTP method to Request class and constant variables to APIBaseTestCase class

--- marketplace/core/tests/base.py
+++ marketplace/core/tests/base.py
@@ -16,12 +16,16 @@ class Request(object):
         self.test_instance = test_instance
         self.factory = APIRequestFactory()
         self.set_view(test_instance.view)
+        self.set_user(test_instance.user)
 
     def get_response_data(self, response: Response) -> dict:
         return json.loads(json.dumps(response.data))
 
     def set_view(self, view):
         self._view = view
+
+    def set_user(self, user):
+        self._user = user
 
     def _get_response(self, request, **kwargs) -> Response:
         response = self._view(request, **kwargs)
@@ -31,28 +35,37 @@ class Request(object):
 
     def get(self, url: str, **kwargs) -> Response:
         request = self.factory.get(url)
-        force_authenticate(request, user=self.test_instance.user)
+        force_authenticate(request, user=self._user)
 
         return self._get_response(request, **kwargs)
 
-    def post(self, url, body=None, **kwargs) -> Response:
+    def post(self, url: str, body=None, **kwargs) -> Response:
         request = self.factory.post(url, data=json.dumps(body), content_type="application/json")
-        force_authenticate(request, user=self.test_instance.user)
+        force_authenticate(request, user=self._user)
 
         return self._get_response(request, **kwargs)
 
-    def put(self,url, body=None, **kwargs):
+    def put(self, url: str, body=None, **kwargs) -> Response:
         request = self.factory.put(url, data=json.dumps(body), content_type="application/json")
-        force_authenticate(request, user=self.test_instance.user)
+        force_authenticate(request, user=self._user)
 
         return self._get_response(request, **kwargs)
 
-    def delete(self):
-        # TODO: Implements DELETE method later
-        ...
+    def delete(self, url: str, **kwargs) -> Response:
+        request = self.factory.delete(url, content_type="application/json")
+        force_authenticate(request, user=self._user)
+
+        return self._get_response(request, **kwargs)
 
 
 class APIBaseTestCase(TestCase):
+
+    ACTION_CREATE = dict(post="create")
+    ACTION_RETRIEVE = dict(get="retrieve")
+    ACTION_UPDATE = dict(put="update")
+    ACTION_DESTROY = dict(delete="destroy")
+    ACTION_LIST = dict(get="list")
+
     url: str
 
     view_class: APIView
