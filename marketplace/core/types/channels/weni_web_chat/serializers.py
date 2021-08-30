10c2b1913a186ca6a7fa59f2a5e71a00eb3e2118
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-08-30 15:43:45 -0300

    fix: Add platform on create a WeniWebChat app

--- marketplace/core/types/channels/weni_web_chat/serializers.py
+++ marketplace/core/types/channels/weni_web_chat/serializers.py
@@ -6,4 +6,10 @@ class WeniWebChatSerializer(AppTypeBaseSerializer):
     class Meta:
         model = App
         fields = ("app_code", "uuid", "project_uuid", "platform", "config", "created_by", "created_on", "modified_by")
-        read_only_fields = ("app_code", "uuid")
+        read_only_fields = ("app_code", "uuid", "platform")
+
+    def create(self, validated_data):
+        from .type import WeniWebChatType
+
+        validated_data["platform"] = WeniWebChatType.platform
+        return super().create(validated_data)
--- marketplace/core/types/channels/weni_web_chat/tests/test_views.py
+++ marketplace/core/types/channels/weni_web_chat/tests/test_views.py
@@ -15,10 +15,7 @@ class CreateWeniWebChatAppTestCase(APIBaseTestCase):
     def setUp(self):
         super().setUp()
 
-        self.body = {
-            "project_uuid": str(uuid.uuid4()),
-            "platform": App.PLATFORM_WENI_FLOWS,
-        }
+        self.body = {"project_uuid": str(uuid.uuid4())}
 
     @property
     def view(self):
@@ -35,12 +32,9 @@ class CreateWeniWebChatAppTestCase(APIBaseTestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("project_uuid", response.json)
 
-    def test_create_app_without_platform(self):
-        self.body.pop("platform")
+    def test_create_app_platform(self):
         response = self.request.post(self.url, self.body)
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("platform", response.json)
+        self.assertEqual(response.json["platform"], App.PLATFORM_WENI_FLOWS)
 
 
 class RetrieveWeniWebChatAppTestCase(APIBaseTestCase):
