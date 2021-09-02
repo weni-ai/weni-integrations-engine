5e5a0bc67c99a9d5aab45e77c820f7b8bb75d869
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-09-02 18:46:26 -0300

    feat: Add CORS_ALLOWED_ORIGINS environment variable

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -200,7 +200,7 @@ if USE_OIDC:
 
 # django-cors-headers Configurations
 
-# TODO: Configure CORS to production environment
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default="")
 CORS_ALLOW_ALL_ORIGINS = DEBUG
 
 
