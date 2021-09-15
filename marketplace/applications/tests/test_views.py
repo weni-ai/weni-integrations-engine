ad360f0d10dc83024b8843d24c2d074edfb31e38
sandro.meireles <sandro.meirelesdev@gmail.com>
2021-09-15 09:20:24 -0300

    feat: Add ProjectAuthorization and set permission to MyAppViewSet

--- marketplace/applications/tests/test_views.py
+++ marketplace/applications/tests/test_views.py
@@ -1,3 +1,4 @@
+from os import stat
 import uuid
 
 from django.test import override_settings
@@ -179,6 +180,7 @@ class RetrieveMyAppViewTestCase(AppTypeViewTestCase):
             platform=App.PLATFORM_WENI_FLOWS,
             created_by=self.user,
         )
+        self.user_authorization = self.user.authorizations.create(project_uuid=self.app.project_uuid)
         self.url = reverse("my-app-detail", kwargs={"uuid": self.app.uuid})
 
     @property
@@ -215,11 +217,12 @@ class ListMyAppViewTestCase(AppTypeViewTestCase):
         super().setUp()
         self.apps = []
 
-        self.common_uuid = uuid.uuid4()
+        self.project_uuid = uuid.uuid4()
         self.apps_count = 10
+        self.user_authorization = self.user.authorizations.create(project_uuid=self.project_uuid)
 
         for num in range(self.apps_count):
-            app_args = [self.common_uuid]
+            app_args = [self.project_uuid]
             if num % 2 == 1:
                 app_args.append({})
 
@@ -239,11 +242,11 @@ class ListMyAppViewTestCase(AppTypeViewTestCase):
         )
 
     def test_request_status_ok(self):
-        response = self.request.get(self.url + f"?project_uuid={self.common_uuid}")
+        response = self.request.get(self.url + f"?project_uuid={self.project_uuid}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_apps_count_by_project(self):
-        response = self.request.get(self.url + f"?project_uuid={self.common_uuid}")
+        response = self.request.get(self.url + f"?project_uuid={self.project_uuid}")
         self.assertEqual(len(response.json), self.apps_count)
 
     def test_apps_count_with_another_project_uuid(self):
@@ -259,20 +262,25 @@ class ListMyAppViewTestCase(AppTypeViewTestCase):
         self.assertEqual(response.json[0], "project_uuid is a required parameter!")
 
     def test_configured_equal_true_filter(self):
-        response = self.request.get(self.url + f"?project_uuid={self.common_uuid}" + "&configured=true")
+        response = self.request.get(self.url + f"?project_uuid={self.project_uuid}" + "&configured=true")
         for app in response.json:
             self.assertNotEqual(app["config"], {})
 
         self.assertEqual(len(response.json), 5)
 
     def test_configured_equal_false_filter(self):
-        response = self.request.get(self.url + f"?project_uuid={self.common_uuid}" + "&configured=false")
+        response = self.request.get(self.url + f"?project_uuid={self.project_uuid}" + "&configured=false")
         for app in response.json:
             self.assertEqual(app["config"], {})
 
         self.assertEqual(len(response.json), 5)
 
     def test_configured_wrong_data(self):
-        response = self.request.get(self.url + f"?project_uuid={self.common_uuid}" + "&configured=test")
+        response = self.request.get(self.url + f"?project_uuid={self.project_uuid}" + "&configured=test")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json[0], "Expected a boolean param in configured, but recived `test`")
+
+    def test_request_without_permission(self):
+        self.user_authorization.delete()
+        response = self.request.get(self.url + f"?project_uuid={self.project_uuid}")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
