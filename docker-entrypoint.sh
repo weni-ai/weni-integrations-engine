bcf5ff66e21af5c7030a5f9b069374cc9baade04
Sandro Meireles <sandro.meirelesdev@gmail.com>
2021-11-03 18:19:07 -0300

    feat: Run migration automatically

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,8 @@
 echo "Running collectstatic"
 python manage.py collectstatic --noinput
 
+echo "Running migrate"
+python manage.py migrate
+
 echo "Starting server"
 gunicorn marketplace.wsgi -c gunicorn.conf.py
