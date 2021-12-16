420c76be45bba2887a83570b87c0ac6637ad3021
Sandro Meireles <sandro.meirelesdev@gmail.com>
2021-12-16 14:35:04 -0300

    feat: Add WhatsAppDemoType environment variables

--- marketplace/core/types/channels/whatsapp_demo/type.py
+++ marketplace/core/types/channels/whatsapp_demo/type.py
@@ -1,14 +1,22 @@
+from decouple import config
+
 from marketplace.core.types.base import AppType
 from marketplace.applications.models import App
-
 from .views import WhatsAppDemoViewSet
 
 
 class WhatsAppDemoType(AppType):
     view_class = WhatsAppDemoViewSet
 
+    NUMBER = config("ROUTER_NUMBER")
+    COUNTRY = config("ROUTER_COUNTRY", "BR")
+    BASE_URL = config("ROUTER_BASE_URL")
+    USERNAME = config("ROUTER_USERNAME")
+    PASSWORD = config("ROUTER_PASSWORD")
+    FACEBOOK_NAMESPACE = config("ROUTER_FACEBOOK_NAMESPACE")
+
     code = "wpp-demo"
-    channeltype_code = None
+    channeltype_code = "WA"
     name = "WhatsApp Demo"
     description = "WhatsAppDemo.data.description"
     summary = "WhatsAppDemo.data.summary"
