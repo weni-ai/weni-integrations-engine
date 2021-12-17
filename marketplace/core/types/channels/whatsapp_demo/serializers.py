d6278aa82ed3cde8b752fc91701b66ad28e04051
Sandro Meireles <sandro.meirelesdev@gmail.com>
2021-12-17 10:28:22 -0300

    feat: Add redirect_url on WhatsApp Demo serializer

--- marketplace/core/types/channels/whatsapp_demo/serializers.py
+++ marketplace/core/types/channels/whatsapp_demo/serializers.py
@@ -1,11 +1,26 @@
+from rest_framework import serializers
+
 from marketplace.applications.models import App
 from marketplace.core.serializers import AppTypeBaseSerializer
 
 
 class WhatsAppDemoSerializer(AppTypeBaseSerializer):
+
+    redirect_url = serializers.SerializerMethodField()
+
     class Meta:
         model = App
-        fields = ("code", "uuid", "project_uuid", "platform", "config", "created_by", "created_on", "modified_by")
+        fields = (
+            "code",
+            "uuid",
+            "project_uuid",
+            "platform",
+            "config",
+            "created_by",
+            "created_on",
+            "modified_by",
+            "redirect_url",
+        )
         read_only_fields = ("code", "uuid", "platform")
 
     def create(self, validated_data):
@@ -13,3 +28,6 @@ class WhatsAppDemoSerializer(AppTypeBaseSerializer):
 
         validated_data["platform"] = WhatsAppDemoType.platform
         return super().create(validated_data)
+
+    def get_redirect_url(self, instance) -> str:
+        return instance.config.get("redirect_url")
