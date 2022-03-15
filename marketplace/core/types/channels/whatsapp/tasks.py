f5dad163a0699639f5be1fbda1c29f4516fb371b
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-03-15 11:57:17 -0300

    feat: Add task to manage queue status

--- marketplace/core/types/channels/whatsapp/tasks.py
+++ marketplace/core/types/channels/whatsapp/tasks.py
@@ -1,16 +1,20 @@
 import time
 import logging
 
+import requests
 from django.conf import settings
 from django_redis import get_redis_connection
+from requests.exceptions import ConnectionError
 
 from marketplace.celery import app as celery_app
-from .facades import InfrastructureQueueFacade, InfrastructureQueueItem
+from .facades import InfrastructureQueueFacade, QueueItem
+from .exceptions import ItemAlreadyInQueue
 
 logger = logging.getLogger(__name__)
 
 
 # TODO: Create a task that validates if old queue item are deployed
+# TODO: Create a task to remove failed items
 
 
 @celery_app.task(name="manage_queue_size")
@@ -30,18 +34,29 @@ def manage_queue_size():
             return None
         else:
             with redis.lock("infra-lock"):
-                queue_item = InfrastructureQueueItem()
-                queue.deploy_whatsapp(queue_item)
-
-                logger.info(f"Deploying a new infrastructure whose `uid` is {queue_item.uid}")
-                time.sleep(settings.WHATSAPP_TIME_BETWEEN_DEPLOY_INFRASTRUCTURE)
+                try:
+                    queue_item = QueueItem()
+                    queue.deploy_whatsapp(queue_item)
+                    logger.info(f"Deploying a new infrastructure whose `uid` is {queue_item.uid}")
+                    time.sleep(settings.WHATSAPP_TIME_BETWEEN_DEPLOY_INFRASTRUCTURE)
+                except ItemAlreadyInQueue:
+                    pass
 
 
 @celery_app.task(name="manage_infra_queue_status")
 def manage_infra_queue_status():
+
     queue = InfrastructureQueueFacade()
 
-    for queue_item in queue.items.all():
+    for item in queue.items.all():
+        if item.status != QueueItem.STATUS_PROPAGATING:
+            continue
 
-        if queue_item.status == InfrastructureQueueItem.STATUS_PROPAGATING:
-            logger.info(f"Validando se propagou")
+        logger.info(f"Validating if infrastructure with uid `{item.uid}` has already propagated")
+        try:
+            response = requests.get(item.url)
+            if response.status_code < 400:
+                queue.items.update(item, status=QueueItem.STATUS_DONE)
+                logger.info(f"Infrastructure with uid `{item.uid}` has been propagated and is ready to use")
+        except ConnectionError:
+            continue
