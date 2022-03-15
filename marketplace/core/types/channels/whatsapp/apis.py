cd3ce22ffae6511b01b78877b007655e9323f243
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-03-15 11:04:21 -0300

    fix: Refactors infrastructure apis

--- marketplace/core/types/channels/whatsapp/apis.py
+++ marketplace/core/types/channels/whatsapp/apis.py
@@ -1,36 +1,35 @@
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
-from django.conf import settings
 import requests
+from django.conf import settings
+from django.urls import reverse
 
 if TYPE_CHECKING:
-    from .queue import InfrastructureQueueItem
+    from .queue import QueueItem
 
 
 class BaseInfrastructureAPI(ABC):
-
     @property
     def _headers(self) -> dict:
         return {
             "Authorization": f"token {settings.WHATSAPP_GITHUB_ACCESS_TOKEN}",
             "Accept": "application/vnd.github.v3+json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
 
     @property
     def _url(self) -> str:
         return settings.WHATSAPP_DISPATCHES_URL
 
-    def _get_payload(self, item: "InfrastructureQueueItem") -> dict:
+    def _get_payload(self, item: "QueueItem") -> dict:
         return {
             "event_type": self._get_event_type(),
             "client_payload": {
-                "uid": item.get_uid(),
-                "webhook_url": settings.WHATSAPP_DISPATCHES_WEBHOOK_URL,
-                "webhook_id": item.get_uid(),
-                "dry_run": "enable",
-            }
+                "uid": item.uid,
+                "webhook_url": settings.EXTERNAL_URL + reverse("wpp-app-webhook"),
+                "webhook_id": item.uid,
+            },
         }
 
     @abstractmethod
@@ -42,15 +41,15 @@ class InfrastructureDeployAPI(BaseInfrastructureAPI):
     def _get_event_type(self) -> str:
         return "deploy-whatsapp"
 
-    def deploy(self, item: "InfrastructureQueueItem"):
+    def deploy(self, item: "QueueItem") -> None:
         # TODO: Validate status
-        response = requests.post(self._url,  json=self._get_payload(item), headers=self._headers)
+        response = requests.post(self._url, json=self._get_payload(item), headers=self._headers)
 
 
 class InfrastructureRemoveAPI(BaseInfrastructureAPI):
     def _get_event_type(self) -> str:
         return "remove-whatsapp"
 
-    def remove(self, item: "InfrastructureQueueItem"):
+    def remove(self, item: "QueueItem") -> None:
         # TODO: Validate status
-        response = requests.post(self._url,  json=self._get_payload(item), headers=self._headers)
+        response = requests.post(self._url, json=self._get_payload(item), headers=self._headers)
