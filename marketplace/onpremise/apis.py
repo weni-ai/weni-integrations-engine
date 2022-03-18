5c86a9ac835e8a9b85200c511455b1bb07d96543
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-03-18 14:20:38 -0300

    feat: Add Login API and PhoneNumber class

--- marketplace/onpremise/apis.py
+++ marketplace/onpremise/apis.py
@@ -3,12 +3,38 @@ from typing import TYPE_CHECKING
 
 import requests
 from requests.auth import HTTPBasicAuth
+from requests.models import Response
 from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
 
 if TYPE_CHECKING:
     from .queue import QueueItem
+
+
+class PhoneNumber(object):
+    def __init__(self, number_data: dict):
+        self._number_data = number_data
+
+        self.display_number = self._get_display_number()
+
+        self.id = self._get_certificate()
+        self.certificate = self._get_certificate()
+        self.country_code, self.ddd, self.number = self.display_number
+        self.complete_number = self.ddd + self.number
+
+    def _get_display_number(self) -> list:
+        display_phone_number = self._number_data.get("display_phone_number")
+        return display_phone_number.replace("-", "").replace("+", "").split()
+
+    def _get_certificate(self):
+        return self._number_data.get("certificate")
+
+    def _get_id(self):
+        return self._number_data.get("id")
+
+    def __str__(self) -> str:
+        return self.complete_number
 
 
 class BaseOnPremiseManifestAPI(ABC):
@@ -40,6 +66,22 @@ class BaseOnPremiseManifestAPI(ABC):
         pass
 
 
+class BaseOnPremiseUserAPI(ABC):
+    @property
+    def _headers(self) -> dict:
+        return {"Content-Type": "application/json"}
+
+    def _request(self, onpremise_url: str, password: str, json: dict = None) -> Response:
+        response = requests.post(
+            f"{onpremise_url}/v1/users/login", json=json, headers=self._headers, auth=HTTPBasicAuth("admin", password)
+        )
+
+        if response.status_code == status.HTTP_401_UNAUTHORIZED:
+            raise Exception("the password of this on premise has already been changed!")
+
+        return response
+
+
 class OnPremiseDeployManifestAPI(BaseOnPremiseManifestAPI):
     def _get_event_type(self) -> str:
         return "deploy-whatsapp"
@@ -58,17 +100,24 @@ class OnPremiseRemoveManifestAPI(BaseOnPremiseManifestAPI):
         requests.post(self._url, json=self._get_payload(item), headers=self._headers)
 
 
-class OnPremisePasswordAPI(object):
-    @property
-    def _headers(self) -> dict:
-        return {"Content-Type": "application/json"}
-
+class OnPremisePasswordAPI(BaseOnPremiseUserAPI):
     def change(self, onpremise_url: str, new_password: str) -> None:
         data = {"new_password": new_password}
+        self._request(onpremise_url, new_password, data)
 
-        response = requests.post(
-            f"{onpremise_url}/v1/users/login", json=data, headers=self._headers, auth=HTTPBasicAuth("admin", "secret")
-        )
 
-        if response.status_code == status.HTTP_401_UNAUTHORIZED:
-            raise Exception("the password of this on premise has already been changed!")
+class OnPremiseLoginAPI(BaseOnPremiseUserAPI):
+    def get_access_token(self, onpremise_url: str, password: str) -> str:
+        response = self._request(onpremise_url, password)
+        users = response.json().get("users", None)
+
+        if users is not None:
+            return users[0].get("token")
+
+
+class OnPremiseRegistrationAPI(object):
+    def _get_url(self, onpremise_url):
+        return onpremise_url + "/v1/account"
+
+    def _get_headers(self, token: str):
+        return {"Content-Type": "application/json", "Authorization": "Bearer " + token}
--- marketplace/onpremise/facades.py
+++ marketplace/onpremise/facades.py
@@ -1,7 +1,17 @@
+from typing import TYPE_CHECKING
+
 from django.utils.crypto import get_random_string
 
 from .queue import QueueItem, QueueItemManager
-from .apis import OnPremiseDeployManifestAPI, OnPremiseRemoveManifestAPI, OnPremisePasswordAPI
+from .apis import (
+    OnPremiseDeployManifestAPI,
+    OnPremiseRemoveManifestAPI,
+    OnPremisePasswordAPI,
+    OnPremiseLoginAPI,
+)
+
+if TYPE_CHECKING:
+    from .apis import PhoneNumber
 
 
 class OnPremiseQueueFacade(object):
@@ -10,11 +20,11 @@ class OnPremiseQueueFacade(object):
         self._deploy_api = OnPremiseDeployManifestAPI()
         self._remove_api = OnPremiseRemoveManifestAPI()
 
-    def deploy_whatsapp(self, item: QueueItem):
+    def deploy_whatsapp(self, item: QueueItem) -> None:
         self.items.add(item)
         self._deploy_api.deploy(item)
 
-    def remove_whatsapp(self, item: QueueItem):
+    def remove_whatsapp(self, item: QueueItem) -> None:
         self._remove_api.remove(item)
         self.items.remove(item)  # TODO: Remove only after confirmation
 
@@ -27,6 +37,7 @@ class OnPremiseQueueFacade(object):
 class OnPremiseFacade(object):
     def __init__(self) -> None:
         self._password_api = OnPremisePasswordAPI()
+        self._login_api = OnPremiseLoginAPI()
 
     def _get_new_password(self) -> str:
         chars = "wFEdGu9ckN!JVKpSrA8WRHnDzsPU3Ca45q7h2tQbeY_xvjg+f@MyBZTX?-m6"
