d883a9d48857af9b53984e613fc71dfd505909d9
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-04-11 14:46:58 -0300

    wip: Mock WhatsApp WABA data

--- marketplace/core/types/channels/whatsapp/serializers.py
+++ marketplace/core/types/channels/whatsapp/serializers.py
@@ -1,3 +1,4 @@
+from pytz import timezone
 from rest_framework import serializers
 
 from marketplace.applications.models import App
@@ -6,6 +7,18 @@ from marketplace.core.serializers import AppTypeBaseSerializer
 
 class WhatsAppConfigSerializer(serializers.Serializer):
     title = serializers.CharField(required=False)
+
+    # TODO: this is a mock, made just to return fake data. Adjust later.
+    waba = serializers.SerializerMethodField()
+
+    def get_waba(self, obj: App) -> dict:
+        return dict(
+            id="372347354000000",
+            name="Weni Tecnologia - FB 881768118500000",
+            message_behalf_name="Weni Tecnologia",
+            timezone="America/Sao_Paulo",
+            namespace="2ee3daabc_0f8e_0000_ae7c_175b808f916",
+        )
 
     class Meta:
         fields = ("title",)
