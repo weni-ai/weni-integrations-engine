31300ec0db8507e4a9c9e08a3992f9e678105aae
Paulo Abreu <pauloabreucoder@gmail.com>
2022-05-18 17:44:09 -0300

    Create a client to consume connect API

--- marketplace/connect/client.py
+++ marketplace/connect/client.py
@@ -6,8 +6,9 @@ from rest_framework import serializers
 
 from marketplace.celery import app as celery_app
 
-
-class ConnectClient:
+class ConnectAuth:
+    api_url = "http://localhost:8080/v1/"
+    bearer_token = ""
 
     def get_auth_token(self) -> str:
         request = requests.post(
@@ -19,5 +20,44 @@ class ConnectClient:
             },
         )
         token = request.json().get("access_token")
-        return f"Bearer {token}"
-        
+        self.bearer_token = "Bearer {token}"
+
+
+    def auth_header(self, headers: dict = {}) -> dict:
+        if 'Authorization' not in headers:
+            headers['Authorization'] = self.bearer_token
+        return headers
+
+class ConnectProjectClient(ConnectAuth):
+
+    def list_channels(self, channeltype_code: str):
+        json = {
+            "channel_type": channeltype_code
+        }
+        request = requests.get(url=self.base_url + '/organization/project/list_channel/', json=json headers=self.auth_header())
+        return request.text
+
+    def create_channel(self, user: str, project_uuid: str, data: dict, channeltype_code: str) -> str:
+        json = {
+            "user": user,
+            "project_uuid": project_uuid,
+            "data": data,
+            "channeltype_code": channeltype_code
+        }
+        request = requests.post(url=self.base_url + '/organization/project/create_channel/', json=json, headers=self.auth_header())
+
+        return request.text
+
+    def release_channel(self, channel_uuid: str, user_email: str) -> None: #informar o jackson sobre o input
+        json = {
+            "channel_uuid": channel_uuid,
+            "user": user_email,
+            "data": data,
+            "channeltype_code": channeltype_code
+        }
+        request = requests.post(url=self.base_url + '/organization/project/release_channel/', json=json, headers=self.auth_header())
+        return request.text
+
+
+class ConnectChannelClient(ConnectAuth):
+    ...
