022cd5d505458231e241b2579422293f19bec882
Paulo Abreu <pauloabreucoder@gmail.com>
2022-05-23 15:52:47 -0300

    adjust tests

--- marketplace/accounts/tests/test_views.py
+++ marketplace/accounts/tests/test_views.py
@@ -26,10 +26,38 @@ class UserPermissionViewTestCase(BaseTest):
     def test_patch(self):
         data = {
             "user": "test",
-            "project_uuid": self.project_uuid,
+            "project_uuid": str(self.project_uuid),
             "data": "18-05-2022",
             "channeltype_code": "test"
         }
-        patch_user = self.request.patch(url=self.url, project_uuid=self.project_uuid)
+        patch_user = self.request.patch(url=self.url, project_uuid=self.project_uuid, body=data)
 
-        self.assertEqual(patch_user.json["project_uuid"], self.project_uuid)
+        self.assertEqual(patch_user.json["project_uuid"], str(self.project_uuid))
+
+
+class UserViewTestCase(BaseTest):
+    project_uuid = uuid.uuid4()
+    url = reverse("user-list")
+
+    view_class = UserViewSet
+
+    @property
+    def view(self):
+        return self.view_class.as_view({"post": "create"})
+
+    def test_patch(self):
+
+        test_user = User.objects.create(email="test@weni.ai", first_name="User", last_name="Test")
+
+        data = {
+            "email": "test@weni.ai",
+            "photo_url": "",
+            "first_name": "User1",
+            "last_name": "Test"
+        }
+        patch_user = self.request.post(url=self.url, body=data)
+
+        test_user = User.objects.get(email="test@weni.ai")
+
+        self.assertEqual(patch_user.json["first_name"], test_user.first_name)
+
