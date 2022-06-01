0e0ffc672f4bffce286a9ff55e6becd251ac9509
Paulo Abreu <pauloabreucoder@gmail.com>
2022-06-01 14:32:17 -0300

    adjust connect client to work with dict

--- marketplace/connect/tasks.py
+++ marketplace/connect/tasks.py
@@ -6,7 +6,7 @@ from marketplace.celery import app as celery_app
 def create_channel(user: str, project_uuid: str, data: dict, channeltype_code: str):
     client = ConnectProjectClient()
     channel = client.create_channel(user, project_uuid, data, channeltype_code)
-    return dict(uuid=channel.uuid, name=channel.name, config=channel.config, address=channel.address)
+    return dict(uuid=channel.get("uuid"), name=channel.get("name"), config=channel.get("config"), address=channel.get("address"))
 
 
 @celery_app.task(name="release_channel")
