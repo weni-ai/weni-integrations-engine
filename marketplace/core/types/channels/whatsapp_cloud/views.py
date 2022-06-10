02e28b74981aad59c0ccbfe781afad4ce18722d1
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-06-10 12:41:14 -0300

    fix: Adjust app_type reference on WhatsApp cloud view

--- marketplace/core/types/channels/whatsapp_cloud/views.py
+++ marketplace/core/types/channels/whatsapp_cloud/views.py
@@ -116,7 +116,7 @@ class WhatsAppCloudViewSet(
         )
 
         task = celery_app.send_task(
-            name="create_wac_channel", args=[request.user.email, project_uuid, data, self.app_type.channeltype_code]
+            name="create_wac_channel", args=[request.user.email, project_uuid, data, self.type_class.channeltype_code]
         )
         task.wait()
 
