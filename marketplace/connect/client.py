0cbe001841ef35e9b8a372721d118384539a9efd
Paulo Abreu <pauloabreucoder@gmail.com>
2022-07-12 15:00:02 -0300

    fix lint

--- marketplace/connect/client.py
+++ marketplace/connect/client.py
@@ -54,9 +54,16 @@ class ConnectProjectClient(ConnectAuth):
         return json.loads(request.text)
 
     def create_wac_channel(self, user: str, project_uuid: str, phone_number_id: str, config: dict):
-        payload = {"user": user, "project_uuid": str(project_uuid), "config": json.dumps(config), "phone_number_id": phone_number_id}
+        payload = {
+            "user": user,
+            "project_uuid": str(project_uuid),
+            "config": json.dumps(config),
+            "phone_number_id": phone_number_id,
+        }
         response = requests.post(
-            url=self.base_url + "/v1/organization/project/create_wac_channel/", json=payload, headers=self.auth_header()
+            url=self.base_url + "/v1/organization/project/create_wac_channel/",
+            json=payload,
+            headers=self.auth_header(),
         )
         return response.json()
 
