71fb1e7de0e00c7fc54e0182b0162d40dee292a4
Paulo Abreu <pauloabreucoder@gmail.com>
2022-08-04 19:16:12 -0300

    test: Add tests to Template Message models

---
+++ marketplace/wpp_templates/tests/test_models.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from django.test import TestCase
+from rest_framework.exceptions import ValidationError
+
+from marketplace.wpp_templates.models import TemplateMessage, TemplateTranslation, TemplateButton, TemplateHeader
+
+
+class TemplateButtonModelTestCase(TestCase):
+    def setUp(self):
+        self.template_message = TemplateMessage.objects.create(
+            name="teste",
+            category="ACCOUNT_UPDATE",
+            created_on=datetime.now(),
+            template_type="TEXT",
+            namespace="teste-namespace",
+        )
+        self.template_translation = TemplateTranslation.objects.create(
+            template=self.template_message, status="APPROVED", language="pt_br", country="Brasil", variable_count=1
+        )
+        super().setUp()
+
+    def test_create_template_button_url_required_fail(self):
+        with self.assertRaises(ValidationError):
+            TemplateButton.objects.create(template_translation=self.template_translation, button_type="URL")
+
+    def test_create_template_button_phone_number_required_fail(self):
+        with self.assertRaises(ValidationError):
+            TemplateButton.objects.create(template_translation=self.template_translation, button_type="PHONE_NUMBER")
+
+
+class TemplateHeaderModelTestCase(TestCase):
+    def setUp(self):
+        self.template_message = TemplateMessage.objects.create(
+            name="teste",
+            category="ACCOUNT_UPDATE",
+            created_on=datetime.now(),
+            template_type="TEXT",
+            namespace="teste-namespace",
+        )
+        self.template_translation = TemplateTranslation.objects.create(
+            template=self.template_message, status="APPROVED", language="pt_br", country="Brasil", variable_count=1
+        )
+        super().setUp()
+
+    def test_create_template_header_text_required_fail(self):
+        with self.assertRaises(ValidationError):
+            TemplateHeader.objects.create(template_translation=self.template_translation, header_type="URL")
