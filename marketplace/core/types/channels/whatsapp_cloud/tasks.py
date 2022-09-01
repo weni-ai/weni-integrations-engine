fcf7af4be4dce61ae0495e6c7c3c2941e447d0ee
Sandro Meireles <49874732+Sandro-Meireles@users.noreply.github.com>
2022-08-31 21:00:19 -0300

    Adjust task that syncs WhatsApp Cloud channels (#172)
    * fix: Adjust task that syncs WhatsApp Cloud channels

--- marketplace/core/types/channels/whatsapp_cloud/tasks.py
+++ marketplace/core/types/channels/whatsapp_cloud/tasks.py
@@ -22,7 +22,7 @@ SYNC_WHATSAPP_CLOUD_LOCK_KEY = "sync-whatsapp-cloud-lock"
 def sync_whatsapp_cloud_apps():
     apptype = APPTYPES.get("wpp-cloud")
     client = ConnectProjectClient()
-    projects = client.list_channels(apptype.channeltype_code)
+    channels = client.list_channels(apptype.channeltype_code)
 
     redis = get_redis_connection()
 
@@ -30,40 +30,41 @@ def sync_whatsapp_cloud_apps():
         logger.info("The apps are already syncing by another task!")
         return None
 
-    for project in projects:
+    for channel in channels:
 
-        channel_data = project.get("channel_data", {})
-        project_uuid = channel_data.get("project_uuid")
+        project_uuid = channel.get("project_uuid")
 
-        for channel in channel_data.get("channels", []):
-            uuid = channel.get("uuid")
-            address = channel.get("address")
-            user = User.objects.get_admin_user()
+        uuid = channel.get("uuid")
+        address = channel.get("address")
+        user = User.objects.get_admin_user()
 
-            config = json.loads(channel.get("config"))
-            config["title"] = config.get("wa_number")
-            config["wa_phone_number_id"] = address
+        config = json.loads(channel.get("config"))
+        config["title"] = config.get("wa_number")
+        config["wa_phone_number_id"] = address
 
-            # TODO: Add title and address to config
+        # TODO: Add title and address to config
 
-            apps = App.objects.filter(flow_object_uuid=uuid)
+        apps = App.objects.filter(flow_object_uuid=uuid)
 
-            if apps.exists():
-                app = apps.first()
+        if apps.exists():
+            app = apps.first()
 
-                if app.code != apptype.code:
-                    logger.info(f"Migrating an {app.code} to WhatsApp Cloud Type. App: {app.uuid}")
-                    app.code = apptype.code
-
+            if app.code != apptype.code:
+                logger.info(f"Migrating an {app.code} to WhatsApp Cloud Type. App: {app.uuid}")
+                app.code = apptype.code
+                config["config_before_migration"] = app.config
                 app.config = config
-                app.modified_by = user
-                app.save()
-
             else:
-                logger.info(f"Creating a new WhatsApp Cloud app for the flow_object_uuid: {uuid}")
-                apptype.create_app(
-                    created_by=user,
-                    project_uuid=project_uuid,
-                    flow_object_uuid=uuid,
-                    config=config,
-                )
+                app.config.update(config)
+
+            app.modified_by = user
+            app.save()
+
+        else:
+            logger.info(f"Creating a new WhatsApp Cloud app for the flow_object_uuid: {uuid}")
+            apptype.create_app(
+                created_by=user,
+                project_uuid=project_uuid,
+                flow_object_uuid=uuid,
+                config=config,
+            )
