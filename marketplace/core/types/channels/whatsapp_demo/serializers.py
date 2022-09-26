79bc489abe5718e5537c267e11c77ad6061cdc70
Eliton <eliton-jorge@hotmail.com>
2022-09-26 14:52:37 -0300

    create get_flows functionality to return flows disponibility

--- marketplace/core/types/channels/whatsapp_demo/serializers.py
+++ marketplace/core/types/channels/whatsapp_demo/serializers.py
@@ -7,6 +7,7 @@ from marketplace.core.serializers import AppTypeBaseSerializer
 class WhatsAppDemoSerializer(AppTypeBaseSerializer):
 
     redirect_url = serializers.SerializerMethodField()
+    flows = serializers.SerializerMethodField()
 
     class Meta:
         model = App
@@ -20,6 +21,7 @@ class WhatsAppDemoSerializer(AppTypeBaseSerializer):
             "created_on",
             "modified_by",
             "redirect_url",
+            "flows",
         )
         read_only_fields = ("code", "uuid", "platform")
 
@@ -29,5 +31,18 @@ class WhatsAppDemoSerializer(AppTypeBaseSerializer):
         validated_data["platform"] = WhatsAppDemoType.platform
         return super().create(validated_data)
 
+
     def get_redirect_url(self, instance) -> str:
         return instance.config.get("redirect_url")
+
+    def get_flows(self, instance):
+        """
+        Returns entity-related flows
+        """
+        flows = None
+        flows = [{'flow_name':'flow_disponivel_01', 'flow_uuid':'3a624a22-c825-461c-9bcf-2fdf7c38eb74', 'keyword':"olá"}, 
+                 {'flow_name':'flow_disponivel_02', 'flow_uuid':'3a624a22-c825-461c-9bcf-2fdf7c38eb74', 'keyword':"hello"}, 
+                 {'flow_name':'flow_disponivel_03', 'flow_uuid':'3a624a22-c825-461c-9bcf-2fdf7c38eb74', 'keyword':"cupom20"}] 
+        
+        return flows
+
