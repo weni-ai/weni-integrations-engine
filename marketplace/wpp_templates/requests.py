b82bcc5ce85e4351f7032f056d00d9a31c160769
Paulo Abreu <pauloabreucoder@gmail.com>
2022-09-28 18:32:05 -0300

    feat: add task to sync templates

--- marketplace/wpp_templates/requests.py
+++ marketplace/wpp_templates/requests.py
@@ -13,6 +13,28 @@ class TemplateMessageRequest(object):
     def _headers(self) -> dict:
         return {"Authorization": f"Bearer {self._access_token}"}
 
+
+    def list_template_messages(self, waba_id: str) -> dict:
+        params = dict(
+            access_token=settings.WHATSAPP_SYSTEM_USER_ACCESS_TOKEN
+        )
+
+        response = requests.get(url=f"https://graph.facebook.com/v14.0/{waba_id}/message_templates", params=params)
+
+        return response.json()
+
+
+    def get_template_namespace(self, waba_id: str) -> dict:
+        params = dict(
+            fields="message_template_namespace",
+            access_token=settings.WHATSAPP_SYSTEM_USER_ACCESS_TOKEN,
+        )
+
+        response = requests.get(url=f"https://graph.facebook.com/v14.0/{waba_id}", params=params)
+
+        return response.json().get("message_template_namespace")
+    
+
     def create_template_message(self, waba_id: str, name: str, category: str, components: list, language: str) -> dict:
         params = dict(
             name=name, category=category, components=str(components), language=language, access_token=self._access_token
