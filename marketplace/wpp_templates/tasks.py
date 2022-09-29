ddb65a84c336639c1c138f50162d6c3c5766f598
Paulo Abreu <pauloabreucoder@gmail.com>
2022-09-29 16:50:19 -0300

    fix: refresh whatsapp templates skip errors

--- marketplace/wpp_templates/tasks.py
+++ marketplace/wpp_templates/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 
 from django.conf import settings
+from sentry_sdk import capture_exception
 
 from .models import TemplateMessage
 from .requests import TemplateMessageRequest
@@ -25,63 +26,56 @@ def refresh_whatsapp_templates_from_facebook():
 
 
         for template in templates.get("data", []):
+            try:
+                found_template, created = TemplateMessage.objects.get_or_create(
+                    app=app,
+                    name=template.get("name"),
+                    category=template.get("category"),
+                )
 
-            found_template, created = TemplateMessage.objects.get_or_create(
-                app=app,
-                name=template.get("name"),
-                category=template.get("category"),
-            )
+                body = ""
+                footer = ""
+                variable_count = 0
+                country = ""
+                namespace = ""
 
-            print(created)
+                for translation in template.get("components"):
+                    if translation.get("type") == "BODY":
+                        body = translation.get("text", "")
 
-            body = ""
-            footer = ""
-            variable_count = 0
-            country = ""
-            namespace = ""
+                    if translation.get("type") == "FOOTER":
+                        footer = translation.get("text", "")
 
-            for translation in template.get("components"):
-                if translation.get("type") == "BODY":
-                    body = translation.get("text", "")
+                returned_translation, created = TemplateTranslation.objects.get_or_create(
+                    template=found_template,
+                    language=template.get("language"),
+                )
+                returned_translation.body = body
+                returned_translation.footer = footer
+                returned_translation.status = template.get("status")
+                returned_translation.variable_count = 0
+                returned_translation.save()
 
-                if translation.get("type") == "FOOTER":
-                    footer = translation.get("text", "")
-
-            returned_translation, created = TemplateTranslation.objects.get_or_create(
-                template=found_template,
-                #status=template.get("status"),
-                language=template.get("language"),
-                #external_id=template.get("id"),
-                #body=body,
-                #footer=footer,
-                #variable_count=variable_count,
-                #country=country,
-                #namespace=namespace,
-            )
-            returned_translation.body = body
-            returned_translation.footer = footer
-            returned_translation.status = template.get("status")
-            returned_translation.variable_count = 0
-            returned_translation.save()
-
-            for translation in template.get("components"):
-                if translation.get("type") == "HEADER":
-                    returned_header, created = TemplateHeader.objects.get_or_create(
-                        translation=returned_translation,
-                        header_type=translation.get("format"),
-                        #example=translation.get("example", {}).get("header_handle"),
-                    )
-                    returned_header.text=translation.get("text", {})
-                    returned_header.example=translation.get("example", {}).get("header_handle")
-                    returned_header.save()
-
-                if translation.get("type") == "BUTTONS":
-                    for button in translation.get("buttons"):
-                        returned_button, created = TemplateButton.objects.get_or_create(
+                for translation in template.get("components"):
+                    if translation.get("type") == "HEADER":
+                        returned_header, created = TemplateHeader.objects.get_or_create(
                             translation=returned_translation,
-                            button_type=button.get("type"),
-                            text=button.get("text"),
-                            url=button.get("url"),
-                            phone_number=button.get("phone_number"),
+                            header_type=translation.get("format"),
                         )
+                        returned_header.text=translation.get("text", {})
+                        returned_header.example=translation.get("example", {}).get("header_handle")
+                        returned_header.save()
 
+                    if translation.get("type") == "BUTTONS":
+                        for button in translation.get("buttons"):
+                            returned_button, created = TemplateButton.objects.get_or_create(
+                                translation=returned_translation,
+                                button_type=button.get("type"),
+                                text=button.get("text"),
+                                url=button.get("url"),
+                                phone_number=button.get("phone_number"),
+                            )
+
+            except Exception as error:
+                capture_exception(error)
+                continue
