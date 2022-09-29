7cf49f872dd5131edc1eba070bb27b3d2e9a73e3
Paulo Abreu <pauloabreucoder@gmail.com>
2022-09-29 12:13:25 -0300

    test30

--- marketplace/wpp_templates/requests.py
+++ marketplace/wpp_templates/requests.py
@@ -16,7 +16,7 @@ class TemplateMessageRequest(object):
 
     def list_template_messages(self, waba_id: str) -> dict:
         params = dict(
-            access_token=settings.WHATSAPP_SYSTEM_USER_ACCESS_TOKEN
+            access_token=self._access_token,
         )
 
         response = requests.get(url=f"https://graph.facebook.com/v14.0/{waba_id}/message_templates", params=params)
@@ -27,7 +27,7 @@ class TemplateMessageRequest(object):
     def get_template_namespace(self, waba_id: str) -> dict:
         params = dict(
             fields="message_template_namespace",
-            access_token=settings.WHATSAPP_SYSTEM_USER_ACCESS_TOKEN,
+            access_token=self._access_token,
         )
 
         response = requests.get(url=f"https://graph.facebook.com/v14.0/{waba_id}", params=params)
