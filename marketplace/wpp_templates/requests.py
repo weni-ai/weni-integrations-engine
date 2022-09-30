d09cb799a034863d5b9dfde8facbafb41aad5a73
Paulo Abreu <pauloabreucoder@gmail.com>
2022-09-30 15:22:19 -0300

    fix: return Facebook error

--- marketplace/wpp_templates/requests.py
+++ marketplace/wpp_templates/requests.py
@@ -49,10 +49,9 @@ class TemplateMessageRequest(object):
 
     def delete_template_message(self, waba_id: str, name: str) -> bool:
         params = dict(name=name, access_token=self._access_token)
-        response = requests.delete(url=f"https://graph.facebook.com/v14.0/{waba_id}/message_templates", params=params)
+        return requests.delete(url=f"https://graph.facebook.com/v14.0/{waba_id}/message_templates", params=params)
 
-        
-        if response.status_code != 200:
-            raise FacebookApiException(response.json())
+        #if response.status_code != 200:
+        #    raise FacebookApiException(response.json())
 
-        return response.json().get("success", False)
+        #return response.json().get("success", False)
