effd206a4ec2749aeb76b4258461109eb7ab386c
elitonzky <elitonzky>
2022-10-27 10:47:58 -0300

    test: update serializer

--- marketplace/core/types/channels/whatsapp_demo/serializers.py
+++ marketplace/core/types/channels/whatsapp_demo/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
 
 from marketplace.applications.models import App
-from marketplace.core.serializers import AppTypeBaseSerializer
+#from marketplace.core.serializers import AppTypeBaseSerializer
 from marketplace.connect.client import ConnectProjectClient
+
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class AppTypeBaseSerializer(serializers.ModelSerializer):
+
+    created_by = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), default=serializers.CurrentUserDefault(), write_only=True
+    )
+    modified_by = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), default=serializers.CurrentUserDefault(), write_only=True
+    )
+
+    def create(self, validated_data):
+        validated_data.pop("modified_by", None)
+        return super().create(validated_data)
+
+    # def update(self, instance, validated_data):
+    #     validated_data.pop("created_by", None)
+    #     return super().update(instance, validated_data)
 
 
 class WhatsAppDemoSerializer(AppTypeBaseSerializer):
