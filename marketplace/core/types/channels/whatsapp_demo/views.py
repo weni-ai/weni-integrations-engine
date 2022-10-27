8f4d36e14bcf79683f4d7748cdbef4173c44376f
elitonzky <elitonzky>
2022-10-27 16:32:56 -0300

    test update data request

--- marketplace/core/types/channels/whatsapp_demo/views.py
+++ marketplace/core/types/channels/whatsapp_demo/views.py
@@ -45,12 +45,12 @@ class WhatsAppDemoViewSet(views.BaseAppTypeViewSet):
         instance.modified_by = user
         instance.save()
 
-    def partial_update(self, request, *args, **kwargs):
+    def update(self, request, *args, **kwargs):
         """ saves the sent flows_starts inside the config and sends it to the router """
         instance = self.get_object()
         flows_starts = request.data.get("flows_starts")
         if not flows_starts:
-            return Response({'message': f'the flows_starts not found in request data: {request.data}, args : {args} , kwargs: {kwargs}'},
+            return Response({'message': f'the flows_starts not found in request data: {request.data}'},
                             status=status.HTTP_404_NOT_FOUND)
 
         instance.config["flows_starts"] = flows_starts
@@ -61,4 +61,3 @@ class WhatsAppDemoViewSet(views.BaseAppTypeViewSet):
         channel_client.set_flows_starts(flows_starts, instance.flow_object_uuid.hex)
         serializer = self.get_serializer(instance)
         return Response(serializer.data, status.HTTP_201_CREATED)
-    
