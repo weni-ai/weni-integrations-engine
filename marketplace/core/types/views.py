fa7dfc89b3015c8a3b5a6e0cd28c1b2d81aee3ba
Paulo Abreu <pauloabreucoder@gmail.com>
2022-12-13 09:00:13 -0300

    fix: remove grpc verification

--- marketplace/core/types/views.py
+++ marketplace/core/types/views.py
@@ -30,6 +30,6 @@ class BaseAppTypeViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin,
 
         channel_uuid = instance.config.get("channelUuid")
 
-        if channel_uuid is not None and settings.USE_GRPC:
+        if channel_uuid:
             client = ConnectProjectClient()
             client.release_channel(channel_uuid, self.request.user.email)
