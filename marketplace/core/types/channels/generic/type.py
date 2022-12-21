8aa456ef6d4a3f3273697e9b67fd32eb5ef5f01c
elitonzky <elitonzky>
2022-12-21 12:16:58 -0300

    fix: remove function search icon from type.py

--- marketplace/core/types/channels/generic/type.py
+++ marketplace/core/types/channels/generic/type.py
@@ -6,30 +6,7 @@ from marketplace.applications.models import App
 from .views import GenericChannelViewSet
 from marketplace.connect.client import ConnectProjectClient
 
-
-def get_channel_types():
-        response = ConnectProjectClient().list_availables_channels()
-        channel_types = response.json().get("channel_types")
-        if channel_types:
-            for channel in channel_types.keys():
-                attribute_channel_types = channel_types.get(channel)
-                channel_types[channel]["attributes"] = search_icon(attribute_channel_types["attributes"])
-        return channel_types
-
-
-def search_icon(attributes):
-    # apptype_asset = AppTypeAsset.objects.filter(code=attributes.get("code").lower())
-    # if apptype_asset.exists():
-    #     apptype_asset = apptype_asset.first()
-    #     icon_url = apptype_asset.url
-    # else:
-    icon_url = None
-
-    attributes["icon_url"] = icon_url
-    return attributes
-
-
-class GenericType(AppType):    
+class GenericType(AppType):
     view_class = GenericChannelViewSet
     code = "generic"
     channeltype_code = None
@@ -41,6 +18,4 @@ class GenericType(AppType):
     bg_color = "#d1fcc9cc"
     platform = App.PLATFORM_WENI_FLOWS
     config_design = "popup"
-    channels_available = [get_channel_types()]
-
-
+    channels_available = None #[get_channel_types()]
