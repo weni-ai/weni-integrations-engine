4c6d8a360299540ea4eaa86e739374c0fcbd26dd
elitonzky <eliton-jorge@hotmail.com>
2023-01-18 11:32:53 -0300

    feature: create facebook apptype

---
+++ marketplace/core/types/channels/facebook/tests/test_views.py
@@ -0,0 +1,126 @@
+import uuid
+
+from django.urls import reverse
+from django.test import override_settings
+from rest_framework import status
+
+from marketplace.core.tests.base import APIBaseTestCase
+from ..views import FacebookViewSet
+from marketplace.applications.models import App
+from marketplace.accounts.models import ProjectAuthorization
+
+
+class CreateFacebookAppTestCase(APIBaseTestCase):
+    url = reverse("fb-app-list")
+    view_class = FacebookViewSet
+
+    @property
+    def view(self):
+        return self.view_class.as_view(APIBaseTestCase.ACTION_CREATE)
+
+    def setUp(self):
+        super().setUp()
+        project_uuid = str(uuid.uuid4())
+        self.body = {"project_uuid": project_uuid}
+
+        self.user_authorization = self.user.authorizations.create(
+            project_uuid=project_uuid, role=ProjectAuthorization.ROLE_CONTRIBUTOR
+        )
+
+    def test_request_ok(self):
+        response = self.request.post(self.url, self.body)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_app_without_project_uuid(self):
+        self.body.pop("project_uuid")
+        response = self.request.post(self.url, self.body)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_app_platform(self):
+        response = self.request.post(self.url, self.body)
+        self.assertEqual(response.json["platform"], App.PLATFORM_WENI_FLOWS)
+
+    def test_create_app_without_permission(self):
+        self.user_authorization.delete()
+        response = self.request.post(self.url, self.body)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class RetrieveFacebookAppTestCase(APIBaseTestCase):
+    view_class = FacebookViewSet
+
+    def setUp(self):
+        super().setUp()
+
+        self.app = App.objects.create(
+            code="fb", created_by=self.user, project_uuid=str(uuid.uuid4()), platform=App.PLATFORM_WENI_FLOWS
+        )
+        self.user_authorization = self.user.authorizations.create(project_uuid=self.app.project_uuid)
+        self.user_authorization.set_role(ProjectAuthorization.ROLE_ADMIN)
+        self.url = reverse("fb-app-detail", kwargs={"uuid": self.app.uuid})
+
+    @property
+    def view(self):
+        return self.view_class.as_view(APIBaseTestCase.ACTION_RETRIEVE)
+
+    def test_request_ok(self):
+        response = self.request.get(self.url, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_app_data(self):
+        response = self.request.get(self.url, uuid=self.app.uuid)
+        self.assertIn("uuid", response.json)
+        self.assertIn("project_uuid", response.json)
+        self.assertIn("platform", response.json)
+        self.assertIn("created_on", response.json)
+        self.assertEqual(response.json["config"], {})
+
+
+@override_settings(USE_GRPC=False)
+class DestroyFacebookAppTestCase(APIBaseTestCase):
+    view_class = FacebookViewSet
+
+    def setUp(self):
+        super().setUp()
+
+        self.app = App.objects.create(
+            code="fb", created_by=self.user, project_uuid=str(uuid.uuid4()), platform=App.PLATFORM_WENI_FLOWS
+        )
+        self.user_authorization = self.user.authorizations.create(
+            project_uuid=self.app.project_uuid, role=ProjectAuthorization.ROLE_ADMIN
+        )
+        self.url = reverse("fb-app-detail", kwargs={"uuid": self.app.uuid})
+
+    @property
+    def view(self):
+        return self.view_class.as_view(APIBaseTestCase.ACTION_DESTROY)
+
+    def test_destroy_app_ok(self):
+        response = self.request.delete(self.url, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_destroy_with_authorization_contributor(self):
+        self.user_authorization.set_role(ProjectAuthorization.ROLE_CONTRIBUTOR)
+        response = self.request.delete(self.url, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_destroy_with_authorization_contributor_and_another_user(self):
+        self.user_authorization.set_role(ProjectAuthorization.ROLE_CONTRIBUTOR)
+        self.request.set_user(self.super_user)
+        self.super_user.authorizations.create(
+            project_uuid=self.app.project_uuid, role=ProjectAuthorization.ROLE_CONTRIBUTOR
+        )
+
+        response = self.request.delete(self.url, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_destroy_with_authorization_viewer(self):
+        self.user_authorization.set_role(ProjectAuthorization.ROLE_VIEWER)
+        response = self.request.delete(self.url, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_destroy_with_authorization_not_setted(self):
+        self.user_authorization.set_role(ProjectAuthorization.ROLE_NOT_SETTED)
+        response = self.request.delete(self.url, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
---
+++ marketplace/core/types/channels/facebook/type.py
@@ -0,0 +1,19 @@
+from marketplace.core.types.base import AppType
+from marketplace.applications.models import App
+from . import views
+
+
+class FacebookType(AppType):
+    view_class = views.FacebookViewSet
+
+    code = "fb"
+    channeltype_code = "FB"
+    name = "Facebook"
+    description = "facebook.data.description"
+    summary = "facebook.data.summary"
+    category = AppType.CATEGORY_CHANNEL
+    developer = "Weni"
+    bg_color = "#039be533"
+    platform = App.PLATFORM_WENI_FLOWS
+    config_design = "sidebar"
+    channels_available = None
