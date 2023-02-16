9a44550566c1a2b49d9b6d3c68f9c142e441904e
elitonzky <eliton-jorge@hotmail.com>
2023-02-16 10:26:55 -0300

    fix: file reformatted with black

--- marketplace/core/types/channels/generic/views.py
+++ marketplace/core/types/channels/generic/views.py
@@ -12,13 +12,13 @@ from marketplace.core.types import views
 from marketplace.flows.client import FlowsClient
 
 from marketplace.applications.models import AppTypeAsset
-from marketplace.core import types
 
 from . import type as type_
 
 
 class GenericChannelViewSet(views.BaseAppTypeViewSet):
-    """ Generic channel create and listing """
+    """Generic channel create and listing"""
+
     serializer_class = GenericChannelSerializer
 
     def get_queryset(self):
@@ -31,8 +31,7 @@ class GenericChannelViewSet(views.BaseAppTypeViewSet):
 
         if channel_code:
             channel_code = channel_code.strip()
-
-        if not channel_code:
+        else:
             raise serializers.ValidationError('Code not be empty.')
 
         client = FlowsClient()
@@ -42,7 +41,9 @@ class GenericChannelViewSet(views.BaseAppTypeViewSet):
             response = response.json()
             if response.get("attributes"):
                 if response.get("attributes").get("claim_blurb"):
-                    channel_claim_blurb = str(response.get("attributes").get("claim_blurb"))
+                    channel_claim_blurb = str(
+                        response.get("attributes").get("claim_blurb")
+                    )
 
                 if response.get("attributes").get("name"):
                     channel_name = response.get("attributes").get("name")
@@ -57,7 +58,7 @@ class GenericChannelViewSet(views.BaseAppTypeViewSet):
 
     @action(detail=True, methods=["PATCH"])
     def configure(self, request, uuid=None):
-        """ Add the generic channel in weni-flows """
+        """Add the generic channel in weni-flows"""
         self.serializer_class = GenericConfigureSerializer
         data = request.data
         serializer = self.get_serializer(self.get_object(), data=data)
@@ -67,7 +68,7 @@ class GenericChannelViewSet(views.BaseAppTypeViewSet):
 
 
 class DetailChannelType(viewsets.ViewSet):
-    """ Search the details of a channel
+    """Search the details of a channel
 
     Args:
         code_channel
@@ -76,6 +77,7 @@ class DetailChannelType(viewsets.ViewSet):
         attributes:{},
         form:{}
     """
+
     lookup_field = "code_channel"
 
     def retrieve(self, request, code_channel=None):
@@ -84,30 +86,37 @@ class DetailChannelType(viewsets.ViewSet):
         if response.status_code == 200:
             return Response(response.json(), status=response.status_code)
 
-        return Response({'message': 'There was an error in the request'}, status=response.status_code)
+        return Response(
+            {"message": "There was an error in the request"},
+            status=response.status_code,
+        )
 
 
 class GetIcons(viewsets.ViewSet):
-    """ Return a dictionary  with the channel_code as key and its value is the url of the icon
+    """Return a dictionary  with the channel_code as key and its value is the url of the icon
 
-        Returns:
-        {
-            'tg': "url.com",
-            'exemple': "url2.com"
-        }
+    Returns:
+    {
+        'tg': "url.com",
+        'exemple': "url2.com"
+    }
     """
+
     def list(self, request):
         client = FlowsClient()
         response = client.list_channel_types(channel_code=None)
         if response.status_code == 200:
             response = response.json()
             channels_icons = {}
-            for channel in response.get('channel_types').keys():
+            for channel in response.get("channel_types").keys():
                 channels_icons[channel] = search_icon(channel)
 
             return Response(channels_icons)
 
-        return Response({'message': 'There was an error in the request'}, status=response.status_code)
+        return Response(
+            {"message": "There was an error in the request"},
+            status=response.status_code,
+        )
 
 
 class GenericAppTypes(viewsets.ViewSet):
@@ -138,22 +147,21 @@ class GenericAppTypes(viewsets.ViewSet):
         return Response(channel_types, status=response.status_code)
 
 
-
 def search_icon(code):
-    """ Search icon url from a channel_code
+    """Search icon url from a channel_code
 
-        Args:
-            Receive: channel_code
+    Args:
+        Receive: channel_code
 
-        Return:
-            "exemple.url.com"
+    Return:
+        "exemple.url.com"
     """
     apptype_asset = AppTypeAsset.objects.filter(code=code.lower())
     if apptype_asset.exists():
         apptype_asset = apptype_asset.first()
         icon_url = apptype_asset.attachment.url
     else:
-        apptype_asset = AppTypeAsset.objects.filter(code='generic')
+        apptype_asset = AppTypeAsset.objects.filter(code="generic")
         if apptype_asset.exists():
             apptype_asset = apptype_asset.first()
             icon_url = apptype_asset.attachment.url
