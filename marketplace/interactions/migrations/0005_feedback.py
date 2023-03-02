241416e3d365858fd1c674efb1543362f8b9f8d2
Sandro Meireles <sandro.meirelesdev@gmail.com>
2023-03-02 16:56:40 -0300

    fix: Run black in feedback migration

--- marketplace/interactions/migrations/0005_feedback.py
+++ marketplace/interactions/migrations/0005_feedback.py
@@ -9,17 +9,24 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('interactions', '0004_auto_20210901_1640'),
+        ("interactions", "0004_auto_20210901_1640"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Feedback',
+            name="Feedback",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('answer', models.CharField(max_length=255)),
-                ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='feedbacks', to=settings.AUTH_USER_MODEL)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("answer", models.CharField(max_length=255)),
+                ("created_on", models.DateTimeField(auto_now_add=True)),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="feedbacks",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
