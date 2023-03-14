0f8aa6664f06393c366472fed9d547298e86843b
Sandro Meireles <sandro.meirelesdev@gmail.com>
2023-03-13 22:49:57 -0300

    feat: Set Omie view to app type

--- marketplace/core/types/externals/omie/type.py
+++ marketplace/core/types/externals/omie/type.py
@@ -1,13 +1,15 @@
 from ..base import ExternalAppType
-
+from .views import OmieViewSet
 
 class OmieType(ExternalAppType):
-    view_class = None
+    view_class = OmieViewSet
 
     code = "omie"
-    flows_type_code = "omie"
+    channeltype_code = "omie" # TODO: rename this field to flows_type_code
     name = "Omie"
     description = "omie.data.description"
     summary = "omie.data.summary"
     bg_color = None
     developer = "Weni"
+    config_design = ""
+    channels_available = None # TODO: This field is temporary, it needs to be removed in the future
