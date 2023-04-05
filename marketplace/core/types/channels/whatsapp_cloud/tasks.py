7a9cf1586ff5611114a0f6f797f99917c4853e94
Eliton <53982066+elitonzky@users.noreply.github.com>
2023-04-05 17:12:09 -0300

    fix: continue synchronization on errors, display error returned from flow (#249)

--- marketplace/core/types/channels/whatsapp_cloud/tasks.py
+++ marketplace/core/types/channels/whatsapp_cloud/tasks.py
@@ -48,7 +48,9 @@ def sync_whatsapp_cloud_apps():
             app = apps.first()
 
             if app.code != apptype.code:
-                logger.info(f"Migrating an {app.code} to WhatsApp Cloud Type. App: {app.uuid}")
+                logger.info(
+                    f"Migrating an {app.code} to WhatsApp Cloud Type. App: {app.uuid}"
+                )
                 app.code = apptype.code
                 config["config_before_migration"] = app.config
                 app.config = config
@@ -59,7 +61,9 @@ def sync_whatsapp_cloud_apps():
             app.save()
 
         else:
-            logger.info(f"Creating a new WhatsApp Cloud app for the flow_object_uuid: {uuid}")
+            logger.info(
+                f"Creating a new WhatsApp Cloud app for the flow_object_uuid: {uuid}"
+            )
             apptype.create_app(
                 created_by=user,
                 project_uuid=project_uuid,
@@ -70,8 +74,8 @@ def sync_whatsapp_cloud_apps():
 
 @celery_app.task(name="check_apps_uncreated_on_flow")
 def check_apps_uncreated_on_flow():
-    """ Search all wpp-cloud channels that have the flow_object_uuid field empty, 
-        to create the object in flows """
+    """Search all wpp-cloud channels that have the flow_object_uuid field empty,
+    to create the object in flows"""
     apps = App.objects.filter(code="wpp-cloud", flow_object_uuid__isnull=True)
 
     for app in apps:
@@ -90,19 +94,25 @@ def check_apps_uncreated_on_flow():
                 channel = client.create_wac_channel(
                     user_creation.email, project_uuid, wa_phone_number_id, app_config
                 )
+                if not channel.get("uuid"):
+                    logger.error(
+                        f"The flow application was not created for app: {app.uuid} flows error return: {(channel)}"
+                    )
+                    continue
 
                 app.flow_object_uuid = channel["uuid"]
-                app.config["channelUuid"] = channel["uuid"]
                 app.save()
 
             except Exception as e:
                 logger.error(f"Error creating channel for app {app.uuid}: {str(e)}")
+                continue
 
         else:
             logger.info(
                 f"""ProjectAuthorization was not found for user: {str(user_creation)}
                     and project:{str(project_uuid)} on app: {str(app.uuid)}"""
             )
+            continue
 
 
 def has_project_access(user, project_uuid) -> bool:
