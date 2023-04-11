3289778a4d663843743dd18b5ea9caeb4218e416
Sandro Meireles <sandro.meirelesdev@gmail.com>
2023-04-11 15:26:46 -0300

    fix: Run black in ConnectProjectClient

--- marketplace/connect/client.py
+++ marketplace/connect/client.py
@@ -23,7 +23,7 @@ class ConnectAuth:
         return {"Authorization": self.__get_auth_token()}
 
 
-class ConnectProjectClient(ConnectAuth): # TODO: change class name to FlowsRESTClient
+class ConnectProjectClient(ConnectAuth):  # TODO: change class name to FlowsRESTClient
 
     base_url = settings.FLOWS_REST_ENDPOINT
     use_connect_v2 = settings.USE_CONNECT_V2
@@ -33,36 +33,19 @@ class ConnectProjectClient(ConnectAuth): # TODO: change class name to FlowsRESTC
         return self.base_url + endpoint
 
     def list_channels(self, channeltype_code: str) -> list:
-        params = {
-            "channel_type": channeltype_code
-        }
+        params = {"channel_type": channeltype_code}
 
         url = self._get_url("/api/v2/internals/channel/")
 
-        response = requests.get(
-            url=url,
-            params=params,
-            headers=self.auth_header(),
-            timeout=60
-        )
+        response = requests.get(url=url, params=params, headers=self.auth_header(), timeout=60)
         return response.json().get("channels", None)
 
     def create_channel(self, user: str, project_uuid: str, data: dict, channeltype_code: str) -> dict:
-        payload = {
-            "user": user,
-            "org": str(project_uuid),
-            "data": data,
-            "channeltype_code": channeltype_code
-        }
+        payload = {"user": user, "org": str(project_uuid), "data": data, "channeltype_code": channeltype_code}
 
         url = self._get_url("/api/v2/internals/channel/")
 
-        response = requests.post(
-            url=url,
-            json=payload,
-            headers=self.auth_header(),
-            timeout=60
-        )
+        response = requests.post(url=url, json=payload, headers=self.auth_header(), timeout=60)
 
         if response.status_code not in [200, 201]:
             raise ValidationError(f"{response.status_code}: {response.text}")
@@ -82,63 +65,37 @@ class ConnectProjectClient(ConnectAuth): # TODO: change class name to FlowsRESTC
         else:
             url = self.base_url + "/v1/organization/project/create_wac_channel/"
 
-        response = requests.post(
-            url=url,
-            json=payload,
-            headers=self.auth_header(),
-            timeout=60
-        )
+        response = requests.post(url=url, json=payload, headers=self.auth_header(), timeout=60)
 
         return response.json()
 
     def release_channel(self, channel_uuid: str, project_uuid: str, user_email: str) -> None:
         params = {"user": user_email}
         url = self._get_url(f"/api/v2/internals/channel/{channel_uuid}")
-        requests.delete(
-            url=url,
-            params=params,
-            headers=self.auth_header(),
-            timeout=60
-        )
+        requests.delete(url=url, params=params, headers=self.auth_header(), timeout=60)
 
         return None
 
     def get_user_api_token(self, user: str, project_uuid: str):
         params = dict(user=user, org=str(project_uuid))
         url = self._get_url("/api/v2/internals/users/api-token/")
-        response = requests.get(
-            url=url,
-            params=params,
-            headers=self.auth_header(),
-            timeout=60
-        )
+        response = requests.get(url=url, params=params, headers=self.auth_header(), timeout=60)
         return response
 
     def list_availables_channels(self):
         url = self.base_url + "/v1/channel-types"
-        response = requests.get(
-            url=url,
-            headers=self.auth_header(),
-            timeout=60
-        )
+        response = requests.get(url=url, headers=self.auth_header(), timeout=60)
         return response
 
     def detail_channel_type(self, channel_code: str):
         params = {"channel_type_code": channel_code}
         url = self.base_url + "/v1/channel-types"
-        response = requests.get(
-            url=url,
-            params=params,
-            headers=self.auth_header(),
-            timeout=60
-        )
+        response = requests.get(url=url, params=params, headers=self.auth_header(), timeout=60)
         return response
 
     def create_external_service(self, user: str, project_uuid: str, type_fields: dict, type_code: str):
         payload = {"user": user, "project": str(project_uuid), "type_fields": type_fields, "type_code": type_code}
-        response = requests.post(
-            self._get_url("/v1/externals"), json=payload, headers=self.auth_header()
-        )
+        response = requests.post(self._get_url("/v1/externals"), json=payload, headers=self.auth_header())
         return response
 
 
