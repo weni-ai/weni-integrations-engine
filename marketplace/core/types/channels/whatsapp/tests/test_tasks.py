12ab211cc4a0647168ab3098a49b92826bb70cd2
elitonzky <eliton-jorge@hotmail.com>
2023-04-12 11:10:58 -0300

    fix: add redis mock in wpp-cloud and wpp task tests

--- marketplace/core/types/channels/whatsapp/tests/test_tasks.py
+++ marketplace/core/types/channels/whatsapp/tests/test_tasks.py
@@ -1,5 +1,4 @@
 from uuid import uuid4
-from typing import TYPE_CHECKING
 from unittest.mock import patch
 
 from django.test import TestCase
@@ -9,15 +8,21 @@ from marketplace.core.types import APPTYPES
 from ..tasks import sync_whatsapp_apps
 from marketplace.applications.models import App
 
-
-if TYPE_CHECKING:
-    from unittest.mock import MagicMock
+from unittest.mock import MagicMock
 
 User = get_user_model()
 
 
 class SyncWhatsAppAppsTaskTestCase(TestCase):
     def setUp(self) -> None:
+        self.redis_mock = MagicMock()
+        self.redis_mock.get.return_value = None
+
+        lock_mock = MagicMock()
+        lock_mock.__enter__.return_value = None
+        lock_mock.__exit__.return_value = False
+        self.redis_mock.lock.return_value = lock_mock
+
         wpp_type = APPTYPES.get("wpp")
         wpp_cloud_type = APPTYPES.get("wpp-cloud")
 
@@ -49,33 +54,39 @@ class SyncWhatsAppAppsTaskTestCase(TestCase):
             }
         ]
 
+    @patch("marketplace.core.types.channels.whatsapp.tasks.get_redis_connection")
     @patch("marketplace.connect.client.ConnectProjectClient.list_channels")
-    def test_create_new_whatsapp_app(self, list_channel_mock: "MagicMock") -> None:
+    def test_create_new_whatsapp_app(self, list_channel_mock: "MagicMock", mock_redis) -> None:
         project_uuid = str(uuid4())
         flow_object_uuid = str(uuid4())
 
         list_channel_mock.return_value = self._get_mock_value(project_uuid, flow_object_uuid)
+
+        mock_redis.return_value = self.redis_mock
 
         sync_whatsapp_apps()
 
         self.assertTrue(App.objects.filter(flow_object_uuid=flow_object_uuid).exists())
         self.assertTrue(App.objects.filter(project_uuid=project_uuid).exists())
 
+    @patch("marketplace.core.types.channels.whatsapp.tasks.get_redis_connection")
     @patch("marketplace.connect.client.ConnectProjectClient.list_channels")
-    def test_update_app_auth_token(self, list_channel_mock: "MagicMock") -> None:
+    def test_update_app_auth_token(self, list_channel_mock: "MagicMock", mock_redis) -> None:
         list_channel_mock.return_value = self._get_mock_value(
             self.wpp_app.project_uuid, self.wpp_app.flow_object_uuid, config={"auth_token": "54321"}
         )
+        mock_redis.return_value = self.redis_mock
 
         sync_whatsapp_apps()
 
         app = App.objects.get(uuid=self.wpp_app.uuid)
         self.assertEqual(app.config.get("auth_token"), "54321")
 
+    @patch("marketplace.core.types.channels.whatsapp.tasks.get_redis_connection")
     @patch("marketplace.connect.client.ConnectProjectClient.list_channels")
-    def test_channel_migration_from_wpp_cloud_to_wpp(self, list_channel_mock: "MagicMock") -> None:
+    def test_channel_migration_from_wpp_cloud_to_wpp(self, list_channel_mock: "MagicMock", mock_redis) -> None:
         list_channel_mock.return_value = self._get_mock_value(
             self.wpp_cloud_app.project_uuid, self.wpp_cloud_app.flow_object_uuid
         )
-
+        mock_redis.return_value = self.redis_mock
         sync_whatsapp_apps()
