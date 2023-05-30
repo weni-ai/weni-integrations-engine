116065ed593c41d3b25c64825a124e09d8b455fb
Eliton <53982066+elitonzky@users.noreply.github.com>
2023-05-30 18:27:55 -0300

    Generic AppType structure changes (#290)
    Creates a generic apptype to be inherited in other generic apptypes and changes the name of the generic channel

--- marketplace/core/types/channels/generic/type.py
+++ marketplace/core/types/channels/generic/type.py
@@ -1,17 +1,9 @@
-from marketplace.core.types.base import AppType
-from marketplace.applications.models import App
 from .views import GenericChannelViewSet
 
+from marketplace.core.types.base import AppType, GenericAppType
 
-class GenericType(AppType):
+
+class GenericChannelAppType(GenericAppType):
     view_class = GenericChannelViewSet
-    code = "generic"
-    flows_type_code = None
-    name = "Generic Type"
-    description = "Generic.data.description"
-    summary = "Generic.data.summary"
     category = AppType.CATEGORY_CHANNEL
-    developer = "Weni"
-    bg_color = "#d1fcc9cc"
-    platform = App.PLATFORM_WENI_FLOWS
-    config_design = "popup"
+    code = "generic"
