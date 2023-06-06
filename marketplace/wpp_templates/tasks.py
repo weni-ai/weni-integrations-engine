285a5cc689897a1d9a5d5af8f9a133fd112cf43b
lucaslinhares <lucaslinhares027@gmail.com>
2023-06-06 16:38:27 -0300

    add delete template in sync task

--- marketplace/wpp_templates/tasks.py
+++ marketplace/wpp_templates/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 
 from django.conf import settings
 from sentry_sdk import capture_exception
+from django.db.models import Q
 
 from .models import TemplateMessage
 from .requests import TemplateMessageRequest
@@ -29,6 +30,20 @@ def refresh_whatsapp_templates_from_facebook():
             settings.WHATSAPP_SYSTEM_USER_ACCESS_TOKEN
         )
         templates = template_message_request.list_template_messages(waba_id)
+
+        if waba_id:
+            templates_message = TemplateMessage.objects.filter(
+                Q(app__config__waba__id=waba_id) |
+                Q(app__config__wa_waba_id=waba_id)
+                )
+            if templates_message:
+                templates_ids = [item['id'] for item in templates["data"]]
+                for template in templates_message:
+                    template_translation = TemplateTranslation.objects.filter(template=template)
+                    for translation in template_translation:
+                        if translation.message_template_id not in templates_ids:
+                            translation.delete()
+
         template_message_request.get_template_namespace(waba_id)
         for template in templates.get("data", []):
             try:
