a3d13d49cc18b947ce7b6254937dcf82b6492b10
elitonzky <eliton-jorge@hotmail.com>
2023-07-13 17:32:55 -0300

    fix: changes percentage coverage to number of lines tested

--- contrib/compare_coverage.py
+++ contrib/compare_coverage.py
@@ -1,43 +1,50 @@
 import subprocess
 import requests
-import xml.etree.ElementTree as ET
-
-
-# Read the coverage report from the XML file
-COVERAGE_REPORT = ET.parse("coverage.xml").getroot()
-
-# Get the current coverage rate from the XML file
-COVERAGE_CURRENT = float(COVERAGE_REPORT.attrib["line-rate"])
-COVERAGE_CURRENT_ABSOLUTE = COVERAGE_CURRENT * 100
 
 try:
-    # Get the latest commit ID from the Git repository
-    LAST_COMMIT = (
+    LAST_MAIN_COMMIT = (
         subprocess.check_output(["git", "rev-parse", "origin/main"]).decode().strip()
     )
+    LAST_LOCAL_COMMIT = (
+        subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+    )
 except subprocess.CalledProcessError as e:
-    print("Error:", e.output)
+    print("Error executing git command:", e)
     exit(1)
 
-# Get the coverage rate for the latest commit from the Codecov API
+
 URL_V2 = (
     "https://api.codecov.io/api/v2/gh/weni-ai/repos/weni-integrations-engine/commits/"
 )
-COVERAGE_LAST_COMMIT_API = f"{URL_V2}{LAST_COMMIT}"
-response = requests.get(COVERAGE_LAST_COMMIT_API).json()
-COVERAGE_LAST_COMMIT = response["totals"]["coverage"]
 
-# Compare the current coverage rate to the coverage rate for the latest commit
-current_commit_cov = round(float(COVERAGE_CURRENT_ABSOLUTE), 2)
-last_commit_cov = round(float(COVERAGE_LAST_COMMIT), 2)
+response_main_commit = requests.get(f"{URL_V2}{LAST_MAIN_COMMIT}")
+response_local_commit = requests.get(f"{URL_V2}{LAST_LOCAL_COMMIT}")
 
-if current_commit_cov < last_commit_cov:
-    print(
-        f"Coverage decreased from {last_commit_cov} to {current_commit_cov}"
-    )
+if response_main_commit.status_code != 200 or response_local_commit.status_code != 200:
+    print("Error: Failed to fetch coverage data from Codecov API")
     exit(1)
+
+try:
+    response_main_commit = response_main_commit.json()
+    response_local_commit = response_local_commit.json()
+except ValueError as e:
+    print("Error parsing JSON response from Codecov API:", e)
+    exit(1)
+
+if "totals" in response_local_commit and "totals" in response_main_commit:
+    local_misses = response_local_commit["totals"].get("misses")
+    main_misses = response_main_commit["totals"].get("misses")
 else:
-    print(
-        f"Coverage increased from {last_commit_cov} to {current_commit_cov}"
-    )
-    exit(0)
+    print("Error: Invalid JSON response from Codecov API")
+    exit(1)
+
+if local_misses is not None and main_misses is not None:
+    if local_misses > main_misses:
+        print(f"Number of test lines decreased by {local_misses - main_misses}")
+        exit(1)
+    else:
+        print(f"Number of test lines increased by {main_misses - local_misses}")
+        exit(0)
+else:
+    print("Error: Missing 'misses' value in JSON response from Codecov API")
+    exit(1)
