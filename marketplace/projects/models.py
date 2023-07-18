7f687a2eb45a1fb3a154aad776dbff990717856e
Eliton <53982066+elitonzky@users.noreply.github.com>
2023-07-18 17:52:27 -0300

    feat: create Template Type model (#328)

--- marketplace/projects/models.py
+++ marketplace/projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TemplateType(models.Model):
+    uuid = models.UUIDField(editable=False, unique=True)
+    name = models.CharField(max_length=255)
+    setup = models.JSONField(default=dict)
+
+    def __str__(self) -> str:
+        return self.name
