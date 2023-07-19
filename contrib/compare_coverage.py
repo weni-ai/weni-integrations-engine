248092256b90ec7590c65ee708556497a96412b0
elitonzky <eliton-jorge@hotmail.com>
2023-07-19 16:18:15 -0300

    teste 03

--- contrib/compare_coverage.py
+++ contrib/compare_coverage.py
@@ -5,9 +5,6 @@ try:
     LAST_MAIN_COMMIT = (
         subprocess.check_output(["git", "rev-parse", "origin/main"]).decode().strip()
     )
-    LAST_LOCAL_COMMIT = (
-        subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
-    )
 except subprocess.CalledProcessError as e:
     print("Error executing git command:", e)
     exit(1)
@@ -18,36 +15,33 @@ URL_V2 = (
 )
 
 response_main_commit = requests.get(f"{URL_V2}{LAST_MAIN_COMMIT}")
-response_local_commit = requests.get(f"{URL_V2}{LAST_LOCAL_COMMIT}")
 
-if response_main_commit.status_code != 200 or response_local_commit.status_code != 200:
+
+if response_main_commit.status_code != 200:
     print("Error: Failed to fetch coverage data from Codecov API")
     exit(1)
 
 try:
     response_main_commit = response_main_commit.json()
-    response_local_commit = response_local_commit.json()
 except ValueError as e:
     print("Error parsing JSON response from Codecov API:", e)
     exit(1)
 
-if "totals" in response_local_commit and "totals" in response_main_commit:
-    local_misses = response_local_commit["totals"].get("misses")
+if "totals" in response_main_commit:
+    local_misses = int(
+        subprocess.check_output("coverage report -m | awk 'END {print $3}'", shell=True)
+        .decode("utf-8")
+        .replace("\n", "\n ")
+        .strip()
+    )
+
     main_misses = response_main_commit["totals"].get("misses")
 else:
     print("Error: Invalid JSON response from Codecov API")
     exit(1)
 
-local_report = int(
-    subprocess.check_output("coverage report -m | awk 'END {print $3}'", shell=True)
-    .decode("utf-8")
-    .replace("\n", "\n ")
-    .strip()
-)
-
 print(f"local_misses:{local_misses}")
 print(f"main_misses:{main_misses}")
-print(f"local_report:{local_report}")
 
 if local_misses is not None and main_misses is not None:
     if local_misses > main_misses:
