54a249d3024d411599e103822a36742d0b44bc46
Sandro Meireles <sandro.meirelesdev@gmail.com>
2023-07-21 11:20:59 -0300

    feat: Create TemplateType and Project consumers and handle queues

--- marketplace/event_driven/handle.py
+++ marketplace/event_driven/handle.py
@@ -1,5 +1,8 @@
 from amqp.channel import Channel
 
+from .consumers import TemplateTypeConsumer, ProjectConsumer
+
 
 def handle_consumers(channel: Channel) -> None:
-    pass
+    channel.basic_consume("integrations.template-types", callback=TemplateTypeConsumer.consume)
+    channel.basic_consume("integrations.projects", callback=ProjectConsumer.consume)
