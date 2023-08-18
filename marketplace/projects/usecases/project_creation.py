d29114ccfb8cfd8dce892c29f5f45ecd46f1d51b
Sandro Meireles <sandro.meirelesdev@gmail.com>
2023-08-18 09:27:24 -0300

    refactor: refactor the project creation usecase

--- marketplace/projects/usecases/project_creation.py
+++ marketplace/projects/usecases/project_creation.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-
 from django.contrib.auth import get_user_model
 
-from ..models import Project, TemplateType
+from ..models import Project
+from .interfaces import TemplateTypeIntegrationInterface
 from .exceptions import InvalidProjectData
 
 
@@ -14,51 +14,38 @@ class ProjectCreationDTO:
     uuid: str
     name: str
     is_template: bool
-    user_email: str
     date_format: str
     timezone: str
     template_type_uuid: str
 
 
 class ProjectCreationUseCase:
-    def __init__(self, app_setup_handler):
-        self.__app_setup_handler = app_setup_handler
+    def __init__(self, template_type_integration: TemplateTypeIntegrationInterface):
+        self.__template_type_integration = template_type_integration
 
-    def create_project(self, project_dto: ProjectCreationDTO):
-        project: Project = None
-        template_type: TemplateType = None
-        user: User = None
-
-        if project_dto.is_template and project_dto.template_type_uuid is None:
-            raise InvalidProjectData("'template_type_uuid' cannot be empty when 'is_template' is True!")
-
+    def get_user_by_email(self, email: str) -> User:
         try:
-            template_type = TemplateType.objects.get(uuid=project_dto.template_type_uuid)
-        except TemplateType.DoesNotExist:
-            raise InvalidProjectData(f"Template Type with uuid `{project_dto.template_type_uuid}` does not exists!")
-
-        try:
-            user = User.objects.get(email=project_dto.user_email)
+            return User.objects.get(email=email)
         except User.DoesNotExist:
-            raise InvalidProjectData(f"User with email `{project_dto.user_email}` does not exist!")
+            raise InvalidProjectData(f"User with email `{email}` does not exist!")
 
-        project = Project.objects.filter(uuid=project_dto.uuid).first()
-
-        if project:
-            if project_dto.is_template and project.template_type is not None:
-                raise InvalidProjectData(f"The project `{project.uuid}` already has an integrated template!")
-
-        else:
-            project = Project.objects.create(
-                uuid=project_dto.uuid,
+    def get_or_create_project(self, project_dto: ProjectCreationDTO, user: User) -> tuple[Project, bool]:
+        return Project.objects.get_or_create(
+            uuid=project_dto.uuid,
+            defaults=dict(
                 name=project_dto.name,
-                is_template=project_dto.is_template,
                 date_format=project_dto.date_format,
                 timezone=project_dto.timezone,
                 created_by=user,
-            )
+                is_template=project_dto.is_template,
+            ),
+        )
+
+    def create_project(self, project_dto: ProjectCreationDTO, user_email: str) -> None:
+        user = self.get_user_by_email(user_email)
+        project, _ = self.get_or_create_project(project_dto, user)
 
         if project_dto.is_template:
-            self.__app_setup_handler.setup_apps_in_project(project, template_type, user)
-            project.template_type = template_type
-            project.save()
+            self.__template_type_integration.integrate_template_type_in_project(
+                project, project_dto.template_type_uuid, user
+            )
---
+++ marketplace/projects/usecases/tests/test_project_creation.py
@@ -0,0 +1,94 @@
+import uuid
+from unittest.mock import MagicMock
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from ...models import Project
+from ..project_creation import ProjectCreationDTO, ProjectCreationUseCase
+from ..exceptions import InvalidProjectData
+
+
+User = get_user_model()
+
+
+class ProjectCreationTestCase(TestCase):
+    def setUp(self) -> None:
+        self.template_type_integration = MagicMock()
+        self.user = User.objects.create_superuser(email="user@marketplace.ai")
+        self.project = Project.objects.create(uuid=uuid.uuid4(), name="Test Project", created_by=self.user)
+
+    def test_get_user_by_email_raises_invalid_project_data(self):
+        usecase = ProjectCreationUseCase(self.template_type_integration)
+
+        with self.assertRaisesMessage(InvalidProjectData, f"User with email `fake@email.com` does not exist!"):
+            usecase.get_user_by_email("fake@email.com")
+
+    def test_get_user_by_email(self):
+        usecase = ProjectCreationUseCase(self.template_type_integration)
+        user = usecase.get_user_by_email("user@marketplace.ai")
+
+        self.assertEqual(user, self.user)
+
+    def test_creating_template_project_integrate_template_type_in_project_is_called(self):
+        project_uuid = uuid.uuid4()
+        project_dto = ProjectCreationDTO(
+            uuid=project_uuid,
+            name="Fake Project",
+            is_template=True,
+            date_format="fake",
+            timezone="fake",
+            template_type_uuid=None,
+        )
+        usecase = ProjectCreationUseCase(self.template_type_integration)
+        usecase.create_project(project_dto, "user@marketplace.ai")
+
+        self.template_type_integration.integrate_template_type_in_project.assert_called_once()
+        self.assertTrue(Project.objects.filter(uuid=project_uuid).exists())
+
+    def test_create_non_template_project(self):
+        project_uuid = uuid.uuid4()
+        project_dto = ProjectCreationDTO(
+            uuid=project_uuid,
+            name="Fake Project",
+            is_template=False,
+            date_format="fake",
+            timezone="fake",
+            template_type_uuid=None,
+        )
+        usecase = ProjectCreationUseCase(self.template_type_integration)
+        usecase.create_project(project_dto, "user@marketplace.ai")
+
+        self.assertTrue(Project.objects.filter(uuid=project_uuid).exists())
+        self.template_type_integration.integrate_template_type_in_project.assert_not_called()
+
+    def test_get_or_create_project_returns_existing_project(self):
+        project_dto = ProjectCreationDTO(
+            uuid=self.project.uuid,
+            name="Fake Project",
+            is_template=False,
+            date_format="fake",
+            timezone="fake",
+            template_type_uuid=None,
+        )
+
+        usecase = ProjectCreationUseCase(self.template_type_integration)
+        project, created = usecase.get_or_create_project(project_dto, self.user)
+        self.assertEqual(project, self.project)
+        self.assertFalse(created)
+
+    def test_get_or_create_project_with_non_existent_uuid(self):
+        project_uuid = uuid.uuid4()
+        project_dto = ProjectCreationDTO(
+            uuid=project_uuid,
+            name="Fake Project",
+            is_template=False,
+            date_format="fake",
+            timezone="fake",
+            template_type_uuid=None,
+        )
+
+        usecase = ProjectCreationUseCase(self.template_type_integration)
+        project, created = usecase.get_or_create_project(project_dto, self.user)
+        self.assertTrue(created)
+        self.assertEqual(project.uuid, project_uuid)
