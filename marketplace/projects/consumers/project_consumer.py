e961fef2bff2b2b6a64134f71d672c9d1e0b1c8b
Sandro Meireles <sandro.meirelesdev@gmail.com>
2023-08-18 09:28:35 -0300

    feat: Adjust the project consumer to the new project creation usecase format

--- marketplace/projects/consumers/project_consumer.py
+++ marketplace/projects/consumers/project_consumer.py
@@ -1,6 +1,6 @@
 import amqp
 
-from ..usecases import ProjectCreationUseCase, AppSetupHandlerUseCase, ProjectCreationDTO
+from ..usecases import TemplateTypeIntegrationUseCase, ProjectCreationUseCase, AppSetupHandlerUseCase, ProjectCreationDTO
 from marketplace.connect.client import ConnectProjectClient, WPPRouterChannelClient
 from marketplace.event_driven.parsers import JSONParser
 
@@ -15,7 +15,6 @@ class ProjectConsumer:
             uuid=body.get("uuid"),
             name=body.get("name"),
             is_template=body.get("is_template"),
-            user_email=body.get("user_email"),
             date_format=body.get("date_format"),
             template_type_uuid=body.get("template_type_uuid"),
             timezone=body.get("timezone"),
@@ -25,8 +24,9 @@ class ProjectConsumer:
         wpp_router_client = WPPRouterChannelClient()
 
         app_setup_handler = AppSetupHandlerUseCase(connect_client, wpp_router_client)
+        template_type_integration = TemplateTypeIntegrationUseCase(app_setup_handler)
 
-        project_creation = ProjectCreationUseCase(app_setup_handler)
-        project_creation.create_project(project_dto)
+        project_creation = ProjectCreationUseCase(template_type_integration)
+        project_creation.create_project(project_dto, body.get("user_email"))
 
         message.channel.basic_ack(message.delivery_tag)
