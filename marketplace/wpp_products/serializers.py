d2ff1c5bef7cb5afb57fa856216e58c1e82546f1
elitonzky <eliton-jorge@hotmail.com>
2023-10-05 15:50:05 -0300

    fix: sort active catalog to first

--- marketplace/wpp_products/serializers.py
+++ marketplace/wpp_products/serializers.py
@@ -7,10 +7,15 @@ class CatalogSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=True)
     facebook_catalog_id = serializers.CharField(read_only=True)
     category = serializers.CharField(required=True)
+    is_connected = serializers.SerializerMethodField()
 
     class Meta:
         model = Catalog
-        fields = ("uuid", "name", "facebook_catalog_id", "category")
+        fields = ("uuid", "name", "facebook_catalog_id", "category", "is_connected")
+
+    def get_is_connected(self, obj):
+        connected_catalog_id = self.context.get("connected_catalog_id")
+        return obj.facebook_catalog_id == connected_catalog_id
 
 
 class ToggleVisibilitySerializer(serializers.Serializer):
@@ -19,3 +24,25 @@ class ToggleVisibilitySerializer(serializers.Serializer):
 
 class TresholdSerializer(serializers.Serializer):
     treshold = serializers.FloatField()
+
+
+class CatalogListSerializer(serializers.BaseSerializer):
+    def to_representation(self, obj):
+        catalogs = obj.all()
+
+        serialized_data = []
+        connected_data = None
+
+        for catalog in catalogs:
+            serialized_catalog = CatalogSerializer(catalog, context=self.context).data
+
+            if serialized_catalog["is_connected"]:
+                connected_data = serialized_catalog
+            else:
+                serialized_data.append(serialized_catalog)
+
+        # If a connected catalog is found, insert it at the beginning of the list.
+        if connected_data:
+            serialized_data.insert(0, connected_data)
+
+        return serialized_data
