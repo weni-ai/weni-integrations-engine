954adfb5a8c4e16946828e3150016056b717677b
elitonzky <eliton-jorge@hotmail.com>
2023-10-19 14:51:22 -0300

    Create action to activate the analytics template on Facebook

--- marketplace/wpp_templates/analytics/urls.py
+++ marketplace/wpp_templates/analytics/urls.py
@@ -8,7 +8,12 @@ template_analytics = [
         "<uuid:app_uuid>/template-analytics/",
         TemplateAnalyticsViewSet.as_view({"get": "template_analytics"}),
         name="template-analytics",
-    )
+    ),
+    path(
+        "<uuid:app_uuid>/enable-analytics/",
+        TemplateAnalyticsViewSet.as_view({"post": "enable_analytics"}),
+        name="enable-analytics",
+    ),
 ]
 
 urlpatterns = template_analytics
--- marketplace/wpp_templates/analytics/views/views.py
+++ marketplace/wpp_templates/analytics/views/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
+from rest_framework import status
 
 from marketplace.clients.facebook.client import FacebookClient
 from marketplace.wpp_templates.services.facebook import FacebookService
@@ -24,13 +25,26 @@ class TemplateAnalyticsViewSet(viewsets.ViewSet):
 
         return self._fb_service
 
-    @action(detail=True, methods=["GET"])
-    def template_analytics(self, request, app_uuid=None, **kwargs):
-        app = get_object_or_404(App, uuid=app_uuid, code__in=["wpp-cloud", "wpp"])
+    def get_object(self) -> App:
+        app_uuid = self.kwargs.get("app_uuid")
+        return get_object_or_404(App, uuid=app_uuid, code__in=["wpp-cloud", "wpp"])
 
+    @action(detail=True, methods=["GET"])
+    def template_analytics(self, request, **kwargs):
         serializer = AnalyticsSerializer(data=request.query_params)
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
 
-        response = self.fb_service.template_analytics(app, validated_data)
+        response = self.fb_service.template_analytics(self.get_object(), validated_data)
         return Response(response)
+
+    @action(detail=True, methods=["POST"])
+    def enable_analytics(self, request, **kwargs):
+        response = self.fb_service.enable_insights(self.get_object())
+        if response is None:
+            return Response(
+                {"detail": "Fail to active template insights"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(status=status.HTTP_200_OK)
