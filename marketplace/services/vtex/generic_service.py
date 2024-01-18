2f87f69552cfd2f797ce001cb2bc68564366aa7c
lucaslinhares <lucaslinhares027@gmail.com>
2024-01-18 18:07:28 -0300

    add flag to configured vtex app

--- marketplace/services/vtex/generic_service.py
+++ marketplace/services/vtex/generic_service.py
@@ -79,6 +79,7 @@ class VtexService:
         app.config["wpp_cloud_uuid"] = wpp_cloud_uuid
         app.config["initial_sync_completed"] = False
         app.config["title"] = credentials.domain
+        app.config["connected_catalog"] = False
         app.config["rules"] = [
             "exclude_alcoholic_drinks",
             "calculate_by_weight",
