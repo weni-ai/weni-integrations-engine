418ec48d443e4d778917beed2974d508d0fa4319
Eliton Jorge <eliton-jorge@hotmail.com>
2024-02-08 15:51:09 -0300

    Refactoring of the catalog synchronization task (#416)

--- marketplace/wpp_products/tasks.py
+++ marketplace/wpp_products/tasks.py
@@ -1,7 +1,5 @@
 import logging
 
-from functools import wraps
-
 from celery import shared_task
 
 from marketplace.clients.facebook.client import FacebookClient
@@ -26,116 +24,85 @@ SYNC_WHATSAPP_CATALOGS_LOCK_KEY = "sync-whatsapp-catalogs-lock"
 @shared_task(name="sync_facebook_catalogs")
 def sync_facebook_catalogs():
     apptype = APPTYPES.get("wpp-cloud")
-    flows_client = FlowsClient()
-
-    redis = get_redis_connection()
-    if redis.get(SYNC_WHATSAPP_CATALOGS_LOCK_KEY):
-        logger.info("The catalogs are already syncing by another task!")
-        return None
-
-    else:
-        with redis.lock(SYNC_WHATSAPP_CATALOGS_LOCK_KEY):
-            for app in apptype.apps:
-                client = FacebookClient(apptype.get_access_token(app))
-                wa_business_id = app.config.get("wa_business_id")
-                wa_waba_id = app.config.get("wa_waba_id")
-
-                if wa_business_id and wa_waba_id:
-                    local_catalog_ids = set(
-                        app.catalogs.values_list("facebook_catalog_id", flat=True)
-                    )
-
-                    all_catalogs_id, all_catalogs = list_all_catalogs_task(app, client)
-
-                    if all_catalogs_id:
-                        update_catalogs_on_flows_task(app, flows_client, all_catalogs)
-
-                        fba_catalogs_ids = set(all_catalogs_id)
-                        to_create = fba_catalogs_ids - local_catalog_ids
-                        to_delete = local_catalog_ids - fba_catalogs_ids
-
-                        for catalog_id in to_create:
-                            details = get_catalog_details_task(client, catalog_id)
-                            if details:
-                                create_catalog_task(app, details)
-
-                        if to_delete:
-                            delete_catalogs_task(app, to_delete)
+    for app in apptype.apps:
+        service = FacebookCatalogSyncService(app)
+        service.sync_catalogs()
 
 
-def handle_exceptions(
-    logger, error_msg, continue_on_exception=True, extra_info_func=None
-):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
+class FacebookCatalogSyncService:
+    SYNC_WHATSAPP_CATALOGS_LOCK_KEY = "sync-whatsapp-catalogs-lock"
+
+    def __init__(self, app):
+        self.app = app
+        self.client = FacebookClient(app.apptype.get_access_token(app))
+        self.flows_client = FlowsClient()
+        self.redis = get_redis_connection()
+
+    def sync_catalogs(self):
+        if self.redis.get(self.SYNC_WHATSAPP_CATALOGS_LOCK_KEY):
+            print("The catalogs are already syncing by another task!")
+            return
+
+        with self.redis.lock(self.SYNC_WHATSAPP_CATALOGS_LOCK_KEY):
+            wa_business_id = self.app.config.get("wa_business_id")
+            wa_waba_id = self.app.config.get("wa_waba_id")
+
+            if not (wa_business_id and wa_waba_id):
+                print(f"Business ID or WABA ID missing for app: {self.app.uuid}")
+                return
+
             try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                extra_info_str = (
-                    extra_info_func(*args, **kwargs) if extra_info_func else ""
+                local_catalog_ids = set(
+                    self.app.catalogs.values_list("facebook_catalog_id", flat=True)
                 )
-                logger.error(f"{error_msg}{extra_info_str}: {str(e)}")
-                if not continue_on_exception:
-                    raise
+                all_catalogs_id, all_catalogs = self._list_all_catalogs()
 
-        return wrapper
+                if all_catalogs_id:
+                    self._update_catalogs_on_flows(all_catalogs)
+                    self._sync_local_catalogs(all_catalogs_id, local_catalog_ids)
+            except Exception as e:
+                logger.error(f"Error during sync process for App {self.app.name}: {e}")
 
-    return decorator
+    def _list_all_catalogs(self):
+        try:
+            return self.client.list_all_catalogs(self.app.config.get("wa_business_id"))
+        except Exception as e:
+            logger.error(f"Error on list all catalogs for App: {str(e)}")
+            return [], []
 
+    def _update_catalogs_on_flows(self, all_catalogs):
+        try:
+            self.flows_client.update_catalogs(
+                str(self.app.flow_object_uuid), all_catalogs
+            )
+        except Exception as e:
+            logger.error(
+                f"Error updating catalogs on flows for App: {self.app.uuid} error: {str(e)}"
+            )
 
-def get_extra_info(app, *args, **kwargs):
-    return f"- UUID: {app.uuid}"
+    def _sync_local_catalogs(self, all_catalogs_id, local_catalog_ids):
+        fba_catalogs_ids = set(all_catalogs_id)
+        to_create = fba_catalogs_ids - local_catalog_ids
+        to_delete = local_catalog_ids - fba_catalogs_ids
 
+        for catalog_id in to_create:
+            try:
+                details = self.client.get_catalog_details(catalog_id)
+                if details:
+                    Catalog.objects.create(
+                        app=self.app,
+                        facebook_catalog_id=details["id"],
+                        name=details["name"],
+                        category=details["vertical"],
+                    )
+            except Exception as e:
+                logger.error(f"Error creating catalog {catalog_id} for App: {str(e)}")
+                # Continues with the next catalog_id
 
-@handle_exceptions(
-    logger, "Error listing all catalogs for App: ", extra_info_func=get_extra_info
-)
-def list_all_catalogs_task(app, client):
-    try:
-        all_catalog_ids, all_catalogs = client.list_all_catalogs(
-            wa_business_id=app.config.get("wa_business_id")
-        )
-        return all_catalog_ids, all_catalogs
-    except Exception as e:
-        logger.error(f"Error on list all catalogs for App: {str(e)}")
-        return [], []
+        if to_delete:
+            self.app.catalogs.filter(facebook_catalog_id__in=to_delete).delete()
 
-
-@handle_exceptions(
-    logger, "Error updating catalogs for App: ", extra_info_func=get_extra_info
-)
-def update_catalogs_on_flows_task(app, flows_client, all_catalogs):
-    flows_client.update_catalogs(str(app.flow_object_uuid), all_catalogs)
-
-
-@handle_exceptions(
-    logger,
-    "Error getting catalog details for App",
-    continue_on_exception=False,
-    extra_info_func=get_extra_info,
-)
-def get_catalog_details_task(client, catalog_id):
-    return client.get_catalog_details(catalog_id)
-
-
-@handle_exceptions(
-    logger, "Error creating catalog for App: ", extra_info_func=get_extra_info
-)
-def create_catalog_task(app, details):
-    Catalog.objects.create(
-        app=app,
-        facebook_catalog_id=details["id"],
-        name=details["name"],
-        category=details["vertical"],
-    )
-
-
-@handle_exceptions(
-    logger, "Error deleting catalogs for App: ", extra_info_func=get_extra_info
-)
-def delete_catalogs_task(app, to_delete):
-    app.catalogs.filter(facebook_catalog_id__in=to_delete).delete()
+        print(f"Success in synchronizing the app's catalogs for app: {self.app.uuid}")
 
 
 @celery_app.task(name="task_insert_vtex_products")
