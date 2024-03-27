80c097fa521250de22d20e930c6605338af8290f
Eliton Jorge <eliton-jorge@hotmail.com>
2024-03-27 18:35:26 -0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,6 @@ import multiprocessing
 workers = multiprocessing.cpu_count() * 2 + 1
 proc_name = "marketplace"
 default_proc_name = proc_name
-# accesslog = "gunicorn.access"
+accesslog = "gunicorn.access"
 timeout = 120
 bind = "0.0.0.0"
