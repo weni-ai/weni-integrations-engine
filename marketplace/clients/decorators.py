b329b470f06e2ba54d36193e02d5dd9112b2b9a9
elitonzky <eliton-jorge@hotmail.com>
2024-04-26 15:46:23 -0300

    Displays error reason after reaching maximum attempts

--- marketplace/clients/decorators.py
+++ marketplace/clients/decorators.py
@@ -8,13 +8,16 @@ def retry_on_exception(max_attempts=8, start_sleep_time=1, factor=2):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             attempts, sleep_time = 0, start_sleep_time
+            last_exception = ""
             while attempts < max_attempts:
                 try:
                     return func(*args, **kwargs)
                 except requests.exceptions.Timeout as e:
+                    last_exception = e
                     if attempts >= 5:
                         print(f"Timeout error: {str(e)}. Retrying...")
                 except requests.exceptions.HTTPError as e:
+                    last_exception = e
                     if attempts >= 5:
                         if e.response.status_code == 429:
                             print(f"Too many requests: {str(e)}. Retrying...")
@@ -23,6 +26,7 @@ def retry_on_exception(max_attempts=8, start_sleep_time=1, factor=2):
                         else:
                             raise
                 except Exception as e:
+                    last_exception = e
                     if hasattr(e, "status_code") and e.status_code == 404:
                         print(f"Not Found: {str(e)}. Not retrying this.")
                         raise
@@ -39,8 +43,13 @@ def retry_on_exception(max_attempts=8, start_sleep_time=1, factor=2):
                 attempts += 1
                 sleep_time *= factor
 
-            print("Max retry attempts reached. Raising exception.")
-            raise Exception("Rate limit exceeded, max retry attempts reached.")
+            message = (
+                f"Rate limit exceeded, max retry attempts reached. Last error in {func.__name__}:"
+                f"Last error:{last_exception}, after {attempts} attempts."
+            )
+
+            print(message)
+            raise Exception(message)
 
         return wrapper
 
