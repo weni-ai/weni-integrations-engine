43b7c815223ac631cf6a1ed9220739eaf6802415
elitonzky <eliton-jorge@hotmail.com>
2024-05-24 10:27:11 -0300

    No cover rapidpro protocol

--- marketplace/interfaces/rapidpro/protocol.py
+++ marketplace/interfaces/rapidpro/protocol.py
@@ -1,7 +1,7 @@
 from typing import Protocol, Dict, Any
 
 
-class RapidproClientProtocol(Protocol):
+class RapidproClientProtocol(Protocol):  # pragma: no cover
     def send_alert(
         self, incident_name: str, monitor_name: str, details: Dict[str, Any]
     ) -> Any:
