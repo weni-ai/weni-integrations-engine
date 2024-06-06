99835afb4002501a7ce8b6a2dd1ca5a716220dba
elitonzky <eliton-jorge@hotmail.com>
2024-06-06 13:11:36 -0300

    Add generate log method to request base client

--- marketplace/clients/base.py
+++ marketplace/clients/base.py
@@ -35,8 +35,8 @@ class RequestClient:
         )
         if response.status_code >= 400:
             detail = ""
-            logger.error(
-                f"Error on request url {url}", exc_info=1, extra=dict(response=response)
+            self._generate_log(
+                response, url, method, headers, json, data, params, files
             )
             try:
                 detail = response.json()
@@ -45,3 +45,28 @@ class RequestClient:
             raise CustomAPIException(detail=detail, status_code=response.status_code)
 
         return response
+
+    def _generate_log(self, response, url, method, headers, json, data, params, files):
+        request_details = {
+            "method": method,
+            "url": url,
+            "headers": headers,
+            "json": json,
+            "data": data,
+            "params": params,
+            "files": files,
+        }
+        response_details = {
+            "status_code": response.status_code,
+            "headers": dict(response.headers),
+            "body": response.text,
+            "url": response.url,
+        }
+        logger.error(
+            f"Error on request url {url}",
+            exc_info=1,
+            extra={
+                "request_details": request_details,
+                "response_details": response_details,
+            },
+        )
