e46824f0457ef9106235c15ff3da36c413498fc3
Jackson Barbosa <=>
2024-11-28 16:03:20 -0300

    added logs and try three endpoints to get email for gmail

--- marketplace/core/types/emails/gmail/serializers.py
+++ marketplace/core/types/emails/gmail/serializers.py
@@ -1,7 +1,6 @@
 import requests
 import logging
 
-from django.conf import settings
 from rest_framework import serializers
 
 
@@ -21,13 +20,34 @@ class GmailSerializer(serializers.Serializer):
         url = "https://www.googleapis.com/userinfo/v2/me?"
         url += f"access_token={self.validated_data['access_token']}&alt=json&prettyPrint=true"
         response = requests.get(url=url)
+        if response.status_code != 200:
+            print(f"error in first try: {response.json()}")
+            url = "https://www.googleapis.com/oauth2/v2/userinfo?"
+            url += f"access_token={self.validated_data['access_token']}"
+            url += f"&oauth_token={self.validated_data['access_token']}"
+            url += "&alt=json&prettyPrint=true"
+            response = requests.get(url=url)
+            if response.status == 200:
+                print(f"successfull using: {url}")
+            else:
+                print(f"error in second try: {response.json()}")
+                url = "https://www.googleapis.com/oauth2/v1/userinfo?access_token="
+                url += {self.validated_data['access_token']}
+                response = requests.get(url=url)
+                if response == 200:
+                    print(f"sucess using: {url}")
+                else:
+                    print(f"error in third try: {response.json}")
+        else:
+            print(f"sucessfull using: {url}")
+
         base_data = {
             "username": response.json().get("email"),
             "password": "",
-            "smtp_host": settings.SMTP_HOST,
-            "smtp_port": settings.SMTP_PORT,
-            "imap_host": settings.IMAP_HOST,
-            "imap_port": settings.IMAP_PORT,
+            "smtp_host": "smtp.gmail.com",
+            "smtp_port": 587,
+            "imap_host": "imap.gmail.com",
+            "imap_port": 993
         }
         base_data.update(
             {
