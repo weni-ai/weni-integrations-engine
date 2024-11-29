eaa3bf7d4264a3a22ab81104f07ad5f5d90ad2b6
Jackson Barbosa <=>
2024-11-29 14:29:41 -0300

    change the api using to get email

--- marketplace/core/types/emails/gmail/serializers.py
+++ marketplace/core/types/emails/gmail/serializers.py
@@ -17,29 +17,13 @@ class GmailSerializer(serializers.Serializer):
         Extends the base data with Gmail-specific fields.
         """
         # base_data = super().to_channel_data()
-        url = "https://www.googleapis.com/userinfo/v2/me?"
-        url += f"access_token={self.validated_data['access_token']}&alt=json&prettyPrint=true"
-        response = requests.get(url=url)
+        url = "https://gmail.googleapis.com/gmail/v1/users/me/profile"
+        headers = {"Authorization": f"Bearer {self.validated_data['access_token']}"}
+        response = requests.get(url=url, headers=headers)
         if response.status_code != 200:
-            print(f"error in first try: {response.json()}")
-            url = "https://www.googleapis.com/oauth2/v2/userinfo?"
-            url += f"access_token={self.validated_data['access_token']}"
-            url += f"&oauth_token={self.validated_data['access_token']}"
-            url += "&alt=json&prettyPrint=true"
-            response = requests.get(url=url)
-            if response.status_code == 200:
-                print(f"successfull using: {url}")
-            else:
-                print(f"error in second try: {response.json()}")
-                url = "https://www.googleapis.com/oauth2/v1/userinfo?access_token="
-                url += self.validated_data['access_token']
-                response = requests.get(url=url)
-                if response.status_code == 200:
-                    print(f"sucess using: {url}")
-                else:
-                    print(f"error in third try: {response.json}")
+            print(f"error: {response.status_code} - {response.json()}")
         else:
-            print(f"sucessfull using: {url}")
+            print(f"success: {response.json()}")
 
         base_data = {
             "username": response.json().get("email"),
