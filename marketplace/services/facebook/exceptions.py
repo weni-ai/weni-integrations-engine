9441db0a7dacbcadc5be75d0831dc65dbda8fbce
Eliton Jorge <eliton-jorge@hotmail.com>
2024-12-06 19:31:28 -0300

    feat: simulate cart from product_details.SkuSellers (#580)
    * feat: simulate cart from product_details.SkuSellers
    
    * feat: refactoring first_product_synchronization and add product update with batch products (#583)
    
    * feat: refactoring first_product_synchronization and add product update with batch products
    
    * feat: get use_sku_sellers key from app on sync products
    
    * fix: add seller_id into sellers_to_sync list
    
    * fix: add try except on worker and simulate cart to multiple sellers
    
    * fix: adjust parameters on sync with csv file
    
    * fix: remove data_processor parameter from pvt_service on sync seller

--- marketplace/services/facebook/exceptions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status
-from rest_framework.exceptions import APIException
-
-
-class FileNotSendValidationError(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = "The file couldn't be sent. Please try again."
-    default_code = "file_not_be_sent"
