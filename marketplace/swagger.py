3638a7a74933c815d77421a42f5557d84a619b26
Eliton Jorge <eliton-jorge@hotmail.com>
2025-03-17 17:51:04 -0300

    Update from v4.0.12 to v4.0.13 (#633)

--- marketplace/swagger.py
+++ marketplace/swagger.py
@@ -6,7 +6,7 @@ from rest_framework import permissions
 view = get_schema_view(
     openapi.Info(
         title="Integrations API Documentation",
-        default_version="v4.0.12",
+        default_version="v4.0.13",
         desccription="Documentation of the Integrations APIs",
     ),
     public=True,
