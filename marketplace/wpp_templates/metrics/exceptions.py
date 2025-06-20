1a482e56e92b646afa7557434b4bb1b51219a0fc
elitonzky <eliton-jorge@hotmail.com>
2025-06-20 14:28:25 -0300

    refactor: update TemplateMetricsException to inherit from APIException with default status and detail

--- marketplace/wpp_templates/metrics/exceptions.py
+++ marketplace/wpp_templates/metrics/exceptions.py
@@ -1,6 +1,15 @@
-class TemplateMetricsException(Exception):
-    """Raised when expected errors occur in the template metrics use case."""
+# marketplace/wpp_templates/exceptions.py
 
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(message)
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class TemplateMetricsException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "Template metrics processing error."
+    default_code = "template_metrics_error"
+
+    def __init__(self, detail=None, code=None):
+        if detail is None:
+            detail = self.default_detail
+        super().__init__(detail=detail, code=code)
