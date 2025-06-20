4d23df53aa90c052f0af413648ec18f7ec586232
elitonzky <eliton-jorge@hotmail.com>
2025-06-20 14:44:31 -0300

    refactor: simplify TemplateMetricsException by removing default status and detail handling

--- marketplace/wpp_templates/metrics/exceptions.py
+++ marketplace/wpp_templates/metrics/exceptions.py
@@ -1,15 +1,12 @@
-# marketplace/wpp_templates/exceptions.py
-
 from rest_framework.exceptions import APIException
-from rest_framework import status
 
 
 class TemplateMetricsException(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = "Template metrics processing error."
+    status_code = 400
     default_code = "template_metrics_error"
 
-    def __init__(self, detail=None, code=None):
-        if detail is None:
-            detail = self.default_detail
-        super().__init__(detail=detail, code=code)
+    def __init__(self, detail=None):
+        super().__init__(detail)
+
+    def __str__(self):
+        return str(self.detail)
