07881f0802635168a84bb67297df6399a7612a5e
elitonzky <eliton-jorge@hotmail.com>
2026-03-16 14:24:08 -0300

    feat: Add EUR and MXN currency formatting rules for VTEX

---
+++ marketplace/services/vtex/business/rules/currency_eur.py
@@ -0,0 +1,14 @@
+from .interface import Rule
+from typing import Union
+from marketplace.services.vtex.utils.data_processor import FacebookProductDTO
+
+
+class CurrencyEUR(Rule):
+    def apply(self, product: FacebookProductDTO, **kwargs) -> bool:
+        product.price = self.format_price(product.price)
+        product.sale_price = self.format_price(product.sale_price)
+        return True
+
+    @staticmethod
+    def format_price(price: Union[int, float]) -> str:
+        return f"{price / 100:.2f} EUR"
