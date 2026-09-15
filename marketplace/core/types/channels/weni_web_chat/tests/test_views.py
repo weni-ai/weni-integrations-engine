a4fc5eb3ea624aa5823df707ad923082caa2aadb
Matheus Cristian <mathues@email.com>
2026-09-15 17:24:11 -0300

    feat: Add unavailableProductNotify field to ConfigSerializer and corresponding tests

--- marketplace/core/types/channels/weni_web_chat/tests/test_views.py
+++ marketplace/core/types/channels/weni_web_chat/tests/test_views.py
@@ -436,6 +436,49 @@ class ConfigureWeniWebChatTestCase(PermissionTestCaseMixin, APIBaseTestCase):
         "marketplace.core.types.channels.weni_web_chat.serializers.AppStorage",
         MockAppStorage,
     )
+    def test_configure_with_unavailable_product_notify(self, mock_flows_client):
+        self.user_authorization = self.user.authorizations.create(
+            project_uuid=self.app.project_uuid
+        )
+        self.user_authorization.set_role(ProjectAuthorization.ROLE_ADMIN)
+
+        mock_flows_client.return_value.create_channel.return_value = {
+            "uuid": str(uuid.uuid4()),
+        }
+
+        self.body["config"]["unavailableProductNotify"] = True
+        response = self.request.patch(self.url, self.body, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.app.refresh_from_db()
+        self.assertEqual(self.app.config["unavailableProductNotify"], True)
+
+    @patch("marketplace.core.types.channels.weni_web_chat.serializers.FlowsClient")
+    @patch(
+        "marketplace.core.types.channels.weni_web_chat.serializers.AppStorage",
+        MockAppStorage,
+    )
+    def test_configure_without_unavailable_product_notify(self, mock_flows_client):
+        self.user_authorization = self.user.authorizations.create(
+            project_uuid=self.app.project_uuid
+        )
+        self.user_authorization.set_role(ProjectAuthorization.ROLE_ADMIN)
+
+        mock_flows_client.return_value.create_channel.return_value = {
+            "uuid": str(uuid.uuid4()),
+        }
+
+        response = self.request.patch(self.url, self.body, uuid=self.app.uuid)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.app.refresh_from_db()
+        self.assertEqual(self.app.config["unavailableProductNotify"], False)
+
+    @patch("marketplace.core.types.channels.weni_web_chat.serializers.FlowsClient")
+    @patch(
+        "marketplace.core.types.channels.weni_web_chat.serializers.AppStorage",
+        MockAppStorage,
+    )
     def test_configure_with_url_avatar(self, mock_flows_client):
         self.user_authorization = self.user.authorizations.create(
             project_uuid=self.app.project_uuid
